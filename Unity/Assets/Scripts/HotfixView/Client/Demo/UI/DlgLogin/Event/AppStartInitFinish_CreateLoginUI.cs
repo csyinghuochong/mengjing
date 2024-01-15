3772c75556f3070a8295d54fd57fab0b5cf0a249
tangchunguang <394266315@qq.com>
2024-01-15 12:11:22 +0800

    no message

--- Unity/Assets/Scripts/HotfixView/Client/Demo/UI/DlgLogin/Event/AppStartInitFinish_CreateLoginUI.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/UI/DlgLogin/Event/AppStartInitFinish_CreateLoginUI.cs
@@ -6,7 +6,7 @@
         protected override async ETTask Run(Scene root, AppStartInitFinish args)
         {
             Log.Debug("AppStartInitFinish_CreateLoginUI");
-            await root.GetComponent<UIComponent>().ShowWindowAsync(WindowID.WindowID_Login);
+            await root.GetComponent<UIComponent>().ShowWindowAsync(WindowID.WindowID_MJLogin);
         }
     }
 }
