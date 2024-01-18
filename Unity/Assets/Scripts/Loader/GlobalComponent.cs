49edc72d6829497bf48e5d43900cfb538e0a2cf7
FortuneCat0v0 <1142094319@qq.com>
2024-01-18 15:37:42 +0800

    .

--- Unity/Assets/Scripts/Loader/GlobalComponent.cs
+++ Unity/Assets/Scripts/Loader/GlobalComponent.cs
@@ -11,10 +11,10 @@ namespace ET
             self.Global = GameObject.Find("/Global").transform;
             self.Unit = GameObject.Find("/Global/Unit").transform;
             self.UI = GameObject.Find("/Global/UI").transform;
-            self.NormalRoot = GameObject.Find("/Global/UI/Low").transform;
-            self.PopUpRoot = GameObject.Find("/Global/UI/Low").transform;
-            self.FixedRoot = GameObject.Find("/Global/UI/Low").transform;
-            self.OtherRoot = GameObject.Find("/Global/UI/Low").transform;
+            self.NormalRoot = GameObject.Find("/Global/UI/NormalRoot").transform;
+            self.PopUpRoot = GameObject.Find("/Global/UI/PopUpRoot").transform;
+            self.FixedRoot = GameObject.Find("/Global/UI/FixedRoot").transform;
+            self.OtherRoot = GameObject.Find("/Global/UI/OtherRoot").transform;
             self.PoolRoot =  GameObject.Find("/Global/PoolRoot").transform;
             
             self.GlobalConfig = Resources.Load<GlobalConfig>("GlobalConfig");
