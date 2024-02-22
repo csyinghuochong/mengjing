1115637d6675bfbf9cd9e8c83cc4645e2395633f
FortuneCat0v0 <1142094319@qq.com>
2024-02-22 12:11:51 +0800

    (￣ ‘i ￣;)

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_CreateRoleItemViewSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_CreateRoleItemViewSystem.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 
 namespace ET.Client
 {
+    [FriendOf(typeof (PlayerComponent))]
     [EntitySystemOf(typeof (Scroll_Item_CreateRoleItem))]
     public static partial class Scroll_Item_CreateRoleItemSystem
     {
@@ -19,6 +20,48 @@ namespace ET.Client
 
         public static void Refresh(this Scroll_Item_CreateRoleItem self, CreateRoleInfo createRoleInfo)
         {
+            if (createRoleInfo != null)
+            {
+                self.E_RoleNameText.text = createRoleInfo.PlayerName;
+                self.E_RoleLvText.text = GameSettingLanguge.LoadLocalization("等级:") + createRoleInfo.PlayerLv;
+                if (createRoleInfo.OccTwo > 0)
+                {
+                    OccupationTwoConfig occupationTwo = OccupationTwoConfigCategory.Instance.Get(createRoleInfo.OccTwo);
+                    self.E_RoleOccText.text = $"职业:{occupationTwo.OccupationName}";
+                }
+                else
+                {
+                    OccupationConfig occupationConfig = OccupationConfigCategory.Instance.Get(createRoleInfo.PlayerOcc);
+                    self.E_RoleOccText.text = $"职业:{occupationConfig.OccupationName}";
+                }
+
+                self.E_RoleHeadIconImage.sprite = self.Root().GetComponent<ResourcesLoaderComponent>()
+                        .LoadAssetSync<Sprite>(ABPathHelper.GetAtlasPath_2(ABAtlasTypes.PlayerIcon, createRoleInfo.PlayerOcc.ToString()));
+                self.E_RoleHeadIconImage.gameObject.SetActive(true);
+                self.EG_NoRoleRectTransform.gameObject.SetActive(false);
+                self.EG_RoleRectTransform.gameObject.SetActive(true);
+            }
+            else
+            {
+                PlayerComponent playerComponent = self.Root().GetComponent<PlayerComponent>();
+                if (playerComponent.CreateRoleList.Count >= 8)
+                {
+                    self.EG_NoRoleRectTransform.gameObject.SetActive(false);
+                    self.EG_RoleRectTransform.gameObject.SetActive(false);
+                    self.E_RoleLvText.gameObject.SetActive(false);
+                    self.E_RoleHeadIconImage.gameObject.SetActive(false);
+                }
+                else
+                {
+                    //显示为空
+                    self.EG_NoRoleRectTransform.gameObject.SetActive(true);
+                    self.EG_RoleRectTransform.gameObject.SetActive(false);
+                    self.E_RoleNameText.text = GameSettingLanguge.LoadLocalization("点击创建角色");
+                    self.E_RoleLvText.gameObject.SetActive(false);
+                    self.E_RoleHeadIconImage.gameObject.SetActive(false);
+                    self.E_RoleOccText.text = "职业:战士/法师";
+                }
+            }
         }
     }
 }
--- Unity/Assets/Scripts/ModelView/Client/MengJing/UIItemBehaviour/Item_CreateRoleItem.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIItemBehaviour/Item_CreateRoleItem.cs
@@ -19,12 +19,220 @@ namespace ET.Client
 			return this;
 		}
 
+		public UnityEngine.RectTransform EG_RoleRectTransform
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_EG_RoleRectTransform == null )
+     				{
+		    			this.m_EG_RoleRectTransform = UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"EG_Role");
+     				}
+     				return this.m_EG_RoleRectTransform;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"EG_Role");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_RoleOccText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_RoleOccText == null )
+     				{
+		    			this.m_E_RoleOccText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"EG_Role/E_RoleOcc");
+     				}
+     				return this.m_E_RoleOccText;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"EG_Role/E_RoleOcc");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_RoleNameText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_RoleNameText == null )
+     				{
+		    			this.m_E_RoleNameText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"EG_Role/E_RoleName");
+     				}
+     				return this.m_E_RoleNameText;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"EG_Role/E_RoleName");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_RoleHeadIconImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_RoleHeadIconImage == null )
+     				{
+		    			this.m_E_RoleHeadIconImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"EG_Role/E_RoleHeadIcon");
+     				}
+     				return this.m_E_RoleHeadIconImage;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"EG_Role/E_RoleHeadIcon");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_RoleLvText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_RoleLvText == null )
+     				{
+		    			this.m_E_RoleLvText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"EG_Role/E_RoleLv");
+     				}
+     				return this.m_E_RoleLvText;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"EG_Role/E_RoleLv");
+     			}
+     		}
+     	}
+
+		public UnityEngine.RectTransform EG_NoRoleRectTransform
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_EG_NoRoleRectTransform == null )
+     				{
+		    			this.m_EG_NoRoleRectTransform = UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"EG_NoRole");
+     				}
+     				return this.m_EG_NoRoleRectTransform;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"EG_NoRole");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_SelectRoleButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_SelectRoleButton == null )
+     				{
+		    			this.m_E_SelectRoleButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_SelectRole");
+     				}
+     				return this.m_E_SelectRoleButton;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_SelectRole");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_SelectRoleImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_SelectRoleImage == null )
+     				{
+		    			this.m_E_SelectRoleImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_SelectRole");
+     				}
+     				return this.m_E_SelectRoleImage;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_SelectRole");
+     			}
+     		}
+     	}
+
 		public void DestroyWidget()
 		{
+			this.m_EG_RoleRectTransform = null;
+			this.m_E_RoleOccText = null;
+			this.m_E_RoleNameText = null;
+			this.m_E_RoleHeadIconImage = null;
+			this.m_E_RoleLvText = null;
+			this.m_EG_NoRoleRectTransform = null;
+			this.m_E_SelectRoleButton = null;
+			this.m_E_SelectRoleImage = null;
 			this.uiTransform = null;
 			this.DataId = 0;
 		}
 
+		private UnityEngine.RectTransform m_EG_RoleRectTransform = null;
+		private UnityEngine.UI.Text m_E_RoleOccText = null;
+		private UnityEngine.UI.Text m_E_RoleNameText = null;
+		private UnityEngine.UI.Image m_E_RoleHeadIconImage = null;
+		private UnityEngine.UI.Text m_E_RoleLvText = null;
+		private UnityEngine.RectTransform m_EG_NoRoleRectTransform = null;
+		private UnityEngine.UI.Button m_E_SelectRoleButton = null;
+		private UnityEngine.UI.Image m_E_SelectRoleImage = null;
 		public Transform uiTransform = null;
 	}
 }
