bf2a75d65eded6ec266bdb2a62c8951f42d51ae8
tangchunguang <394266315@qq.com>
2024-02-26 17:29:13 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/Activity/C2A_FirstWinInfoHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Activity/C2A_FirstWinInfoHandler.cs
@@ -4,7 +4,7 @@ namespace ET.Server
 {
     [MessageHandler(SceneType.Activity)]
     [FriendOf(typeof(ActivityServerComponent))]
-    [FriendOfAttribute(typeof(ET.Server.DBDayActivityInfo))]
+    [FriendOf(typeof(DBDayActivityInfo))]
     public class C2A_FirstWinInfoHandler : MessageHandler<Scene, C2A_FirstWinInfoRequest, A2C_FirstWinInfoResponse>
     {
         protected override async ETTask Run(Scene scene, C2A_FirstWinInfoRequest request, A2C_FirstWinInfoResponse response)
