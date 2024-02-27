fb4aaddd705c9f2b13c453b846360b31240fe639
tangchunguang <394266315@qq.com>
2024-02-27 12:25:34 +0800

    no message

---
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Map/C2M_PickItemHandler.cs
@@ -0,0 +1,14 @@
+namespace ET.Server
+{
+    [MessageLocationHandler(SceneType.Map)]
+    [FriendOf(typeof (PetComponentServer))]
+    [FriendOf(typeof (BagComponentServer))]
+    public class C2M_PickItemHandler: MessageLocationHandler<Unit, C2M_PickItemRequest, M2C_PickItemResponse>
+    {
+        protected override async ETTask Run(Unit unit, C2M_PickItemRequest request, M2C_PickItemResponse response)
+        {
+            
+            await ETTask.CompletedTask;
+        }
+    }
+}
