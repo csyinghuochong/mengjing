89ab8ef7f25d3a2bc7b94cb3bc77cb89f57643b5
FortuneCat0v0 <1142094319@qq.com>
2024-02-28 12:50:26 +0800

    .

---
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/Help/ErrorViewHelp.cs
@@ -0,0 +1,29 @@
+﻿namespace ET.Client
+{
+    public static class ErrorViewHelp
+    {
+        public static string GetErrorHint(int code)
+        {
+            if (code == 0)
+            {
+                return "";
+            }
+
+            string hintStr = code.ToString();
+            ErrorViewData.ErrorHints.TryGetValue(code, out hintStr);
+            return hintStr;
+        }
+
+        public static void ShowErrorHint(Scene root, int code)
+        {
+            if (code == 0)
+            {
+                return;
+            }
+
+            string hintStr = code.ToString();
+            ErrorViewData.ErrorHints.TryGetValue(code, out hintStr);
+            root.GetComponent<FlyTipComponent>().SpawnFlyTipDi(string.IsNullOrEmpty(hintStr)? code.ToString() : hintStr);
+        }
+    }
+}
