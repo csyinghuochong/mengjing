752099007d69667ad7879a1eadd08f7de1b84ff9
tangchunguang <394266315@qq.com>
2024-03-01 17:09:09 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/Friend/C2F_FriendInfoHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Friend/C2F_FriendInfoHandler.cs
@@ -8,6 +8,14 @@ namespace  ET.Server
         protected override async ETTask Run(Scene scene, C2F_FriendInfoRequest handler, F2C_FriendInfoResponse response)
         {
             Log.Debug("1111111111111:C2A_ActivityInfoRequest");
+            
+            response.FriendList.Add(new FriendInfo()
+            {
+                PlayerName = "玩家1",
+                Occ = 1,
+                PlayerLevel = 1,
+            });
+            
             await ETTask.CompletedTask;
         }
     }
