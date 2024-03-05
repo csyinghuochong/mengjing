1e8e70b1c0a1882cb489cba957ac4a1904347711
FortuneCat0v0 <1142094319@qq.com>
2024-03-05 18:45:30 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgEquipDuiBiTips/DlgEquipDuiBiTipsSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgEquipDuiBiTips/DlgEquipDuiBiTipsSystem.cs
@@ -27,19 +27,92 @@ namespace ET.Client
 
         public static void OnUpdateEquipUI(this DlgEquipDuiBiTips self, ShowItemTips args)
         {
-            self.View.ES_EquipTips.uiTransform.gameObject.SetActive(true);
-            self.View.ES_EquipTips.RefreshInfo(args.BagInfo, args.ItemOperateEnum, 0, args.EquipList);
+            self.View.ES_EquipTips_1.uiTransform.gameObject.SetActive(true);
+            self.View.ES_EquipTips_1.RefreshInfo(args.BagInfo, args.ItemOperateEnum, 0, args.EquipList);
         }
 
         public static void OnUpdateAppraisalUI(this DlgEquipDuiBiTips self, ShowItemTips args)
         {
-            self.View.ES_ItemAppraisalTips.uiTransform.gameObject.SetActive(true);
-            self.View.ES_ItemAppraisalTips.RefreshInfo(args.BagInfo, args.ItemOperateEnum);
+            self.View.ES_ItemAppraisalTips_1.uiTransform.gameObject.SetActive(true);
+            self.View.ES_ItemAppraisalTips_1.RefreshInfo(args.BagInfo, args.ItemOperateEnum);
         }
 
         public static void OnUpdateDuiBiUI(this DlgEquipDuiBiTips self, BagInfo bagInfo_1, ShowItemTips args, int weight,
         ItemOperateEnum itemOperateEnum)
         {
+            float height_1 = 0;
+            float height_2 = 0;
+            BagInfo bagInfo_2 = args.BagInfo;
+
+            height_1 = self.View.ES_EquipTips_1.E_BackImage.GetComponent<RectTransform>().sizeDelta.y;
+            if (bagInfo_2.IfJianDing == false)
+            {
+                height_2 = self.View.ES_EquipTips_2.E_BackImage.GetComponent<RectTransform>().sizeDelta.y;
+                self.View.ES_EquipTips_2.uiTransform.gameObject.SetActive(true);
+            }
+            else
+            {
+                height_2 = self.View.ES_ItemAppraisalTips_2.E_BackImage.GetComponent<RectTransform>().sizeDelta.y;
+                self.View.ES_ItemAppraisalTips_2.uiTransform.gameObject.SetActive(true);
+            }
+
+            Vector2 vectorPoint;
+            GlobalComponent globalComponent = self.Root().GetComponent<GlobalComponent>();
+            RectTransform canvas = globalComponent.NormalRoot.GetComponent<RectTransform>();
+            Camera uiCamera = globalComponent.UICamera.GetComponent<Camera>();
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas, new Vector2(x: args.InputPoint.x, y: args.InputPoint.y), uiCamera,
+                out vectorPoint);
+
+            if (vectorPoint.x <= 0)
+            {
+                if (height_1 > height_2)
+                {
+                    vectorPoint.x += (int)(weight * 0.5);
+                    vectorPoint.y = 0f;
+                    vectorPoint.x -= 50;
+                    self.View.EG_Tips1RectTransform.GetComponent<RectTransform>().anchoredPosition = vectorPoint;
+
+                    vectorPoint.x += 50 + weight;
+                    vectorPoint.y = (height_1 - height_2) * 0.5f;
+                    self.View.EG_Tips2RectTransform.GetComponent<RectTransform>().anchoredPosition = vectorPoint;
+                }
+                else
+                {
+                    vectorPoint.x += (int)(weight * 0.5);
+                    vectorPoint.y = (height_2 - height_1) * 0.5f;
+                    vectorPoint.x -= 50;
+                    self.View.EG_Tips1RectTransform.GetComponent<RectTransform>().anchoredPosition = vectorPoint;
+
+                    vectorPoint.x += weight;
+                    vectorPoint.y = 0f;
+                    self.View.EG_Tips2RectTransform.GetComponent<RectTransform>().anchoredPosition = vectorPoint;
+                }
+            }
+            else
+            {
+                if (height_1 > height_2)
+                {
+                    vectorPoint.x -= (int)(weight * 0.5);
+                    vectorPoint.y = (height_1 - height_2) * 0.5f;
+                    self.View.EG_Tips2RectTransform.GetComponent<RectTransform>().anchoredPosition = vectorPoint;
+
+                    vectorPoint.x -= (20 + weight);
+                    vectorPoint.y = 0f;
+                    vectorPoint.x -= 30;
+                    self.View.EG_Tips1RectTransform.GetComponent<RectTransform>().anchoredPosition = vectorPoint;
+                }
+                else
+                {
+                    vectorPoint.x -= (int)(weight * 0.5);
+                    vectorPoint.y = 0f;
+                    self.View.EG_Tips2RectTransform.GetComponent<RectTransform>().anchoredPosition = vectorPoint;
+
+                    vectorPoint.x -= (20 + weight);
+                    vectorPoint.y = (height_2 - height_1) * 0.5f;
+                    vectorPoint.x -= 30;
+                    self.View.EG_Tips1RectTransform.GetComponent<RectTransform>().anchoredPosition = vectorPoint;
+                }
+            }
         }
     }
 }
--- Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/DlgEquipDuiBiTips/DlgEquipDuiBiTipsViewComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/DlgEquipDuiBiTips/DlgEquipDuiBiTipsViewComponent.cs
@@ -58,7 +58,7 @@ namespace ET.Client
      		}
      	}
 
-		public ES_ItemAppraisalTips ES_ItemAppraisalTips
+		public ES_ItemAppraisalTips ES_ItemAppraisalTips_1
      	{
      		get
      		{
@@ -67,16 +67,16 @@ namespace ET.Client
      				Log.Error("uiTransform is null.");
      				return null;
      			}
-     			if( this.m_es_itemappraisaltips == null )
+     			if( this.m_es_itemappraisaltips_1 == null )
      			{
-		    	   Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"EG_Tips1/ES_ItemAppraisalTips");
-		    	   this.m_es_itemappraisaltips = this.AddChild<ES_ItemAppraisalTips,Transform>(subTrans);
+		    	   Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"EG_Tips1/ES_ItemAppraisalTips_1");
+		    	   this.m_es_itemappraisaltips_1 = this.AddChild<ES_ItemAppraisalTips,Transform>(subTrans);
      			}
-     			return this.m_es_itemappraisaltips;
+     			return this.m_es_itemappraisaltips_1;
      		}
      	}
 
-		public ES_EquipTips ES_EquipTips
+		public ES_EquipTips ES_EquipTips_1
      	{
      		get
      		{
@@ -85,12 +85,12 @@ namespace ET.Client
      				Log.Error("uiTransform is null.");
      				return null;
      			}
-     			if( this.m_es_equiptips == null )
+     			if( this.m_es_equiptips_1 == null )
      			{
-		    	   Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"EG_Tips1/ES_EquipTips");
-		    	   this.m_es_equiptips = this.AddChild<ES_EquipTips,Transform>(subTrans);
+		    	   Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"EG_Tips1/ES_EquipTips_1");
+		    	   this.m_es_equiptips_1 = this.AddChild<ES_EquipTips,Transform>(subTrans);
      			}
-     			return this.m_es_equiptips;
+     			return this.m_es_equiptips_1;
      		}
      	}
 
@@ -111,23 +111,63 @@ namespace ET.Client
      		}
      	}
 
+		public ES_ItemAppraisalTips ES_ItemAppraisalTips_2
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_es_itemappraisaltips_2 == null )
+     			{
+		    	   Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"EG_Tips2/ES_ItemAppraisalTips_2");
+		    	   this.m_es_itemappraisaltips_2 = this.AddChild<ES_ItemAppraisalTips,Transform>(subTrans);
+     			}
+     			return this.m_es_itemappraisaltips_2;
+     		}
+     	}
+
+		public ES_EquipTips ES_EquipTips_2
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_es_equiptips_2 == null )
+     			{
+		    	   Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"EG_Tips2/ES_EquipTips_2");
+		    	   this.m_es_equiptips_2 = this.AddChild<ES_EquipTips,Transform>(subTrans);
+     			}
+     			return this.m_es_equiptips_2;
+     		}
+     	}
+
 		public void DestroyWidget()
 		{
 			this.m_E_CloseButton = null;
 			this.m_E_CloseImage = null;
 			this.m_EG_Tips1RectTransform = null;
-			this.m_es_itemappraisaltips = null;
-			this.m_es_equiptips = null;
+			this.m_es_itemappraisaltips_1 = null;
+			this.m_es_equiptips_1 = null;
 			this.m_EG_Tips2RectTransform = null;
+			this.m_es_itemappraisaltips_2 = null;
+			this.m_es_equiptips_2 = null;
 			this.uiTransform = null;
 		}
 
 		private UnityEngine.UI.Button m_E_CloseButton = null;
 		private UnityEngine.UI.Image m_E_CloseImage = null;
 		private UnityEngine.RectTransform m_EG_Tips1RectTransform = null;
-		private EntityRef<ES_ItemAppraisalTips> m_es_itemappraisaltips = null;
-		private EntityRef<ES_EquipTips> m_es_equiptips = null;
+		private EntityRef<ES_ItemAppraisalTips> m_es_itemappraisaltips_1 = null;
+		private EntityRef<ES_EquipTips> m_es_equiptips_1 = null;
 		private UnityEngine.RectTransform m_EG_Tips2RectTransform = null;
+		private EntityRef<ES_ItemAppraisalTips> m_es_itemappraisaltips_2 = null;
+		private EntityRef<ES_EquipTips> m_es_equiptips_2 = null;
 		public Transform uiTransform = null;
 	}
 }
