d8762ba9eb5a278d3e086a45f5a0a09c7bd691ea
FortuneCat0v0 <1142094319@qq.com>
2024-03-07 19:57:34 +0800

    .

--- Unity/Assets/Scripts/ModelView/Client/MengJing/UIItemBehaviour/Item_FriendListItem.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIItemBehaviour/Item_FriendListItem.cs
@@ -19,12 +19,298 @@ namespace ET.Client
 			return this;
 		}
 
+		public UnityEngine.UI.Image E_HeadIconImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_HeadIconImage == null )
+     				{
+		    			this.m_E_HeadIconImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_HeadIcon");
+     				}
+     				return this.m_E_HeadIconImage;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_HeadIcon");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_PlayerNameText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_PlayerNameText == null )
+     				{
+		    			this.m_E_PlayerNameText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_PlayerName");
+     				}
+     				return this.m_E_PlayerNameText;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_PlayerName");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_PlayerLevelText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_PlayerLevelText == null )
+     				{
+		    			this.m_E_PlayerLevelText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_PlayerLevel");
+     				}
+     				return this.m_E_PlayerLevelText;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_PlayerLevel");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_OnLineTimeText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_OnLineTimeText == null )
+     				{
+		    			this.m_E_OnLineTimeText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_OnLineTime");
+     				}
+     				return this.m_E_OnLineTimeText;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_OnLineTime");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_WatchButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_WatchButton == null )
+     				{
+		    			this.m_E_WatchButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_Watch");
+     				}
+     				return this.m_E_WatchButton;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_Watch");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_WatchImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_WatchImage == null )
+     				{
+		    			this.m_E_WatchImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_Watch");
+     				}
+     				return this.m_E_WatchImage;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_Watch");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_ChatButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_ChatButton == null )
+     				{
+		    			this.m_E_ChatButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_Chat");
+     				}
+     				return this.m_E_ChatButton;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_Chat");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_ChatImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_ChatImage == null )
+     				{
+		    			this.m_E_ChatImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_Chat");
+     				}
+     				return this.m_E_ChatImage;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_Chat");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_DeleteButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_DeleteButton == null )
+     				{
+		    			this.m_E_DeleteButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_Delete");
+     				}
+     				return this.m_E_DeleteButton;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_Delete");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_DeleteImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_DeleteImage == null )
+     				{
+		    			this.m_E_DeleteImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_Delete");
+     				}
+     				return this.m_E_DeleteImage;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_Delete");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_OccNameText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_OccNameText == null )
+     				{
+		    			this.m_E_OccNameText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_OccName");
+     				}
+     				return this.m_E_OccNameText;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_OccName");
+     			}
+     		}
+     	}
+
 		public void DestroyWidget()
 		{
+			this.m_E_HeadIconImage = null;
+			this.m_E_PlayerNameText = null;
+			this.m_E_PlayerLevelText = null;
+			this.m_E_OnLineTimeText = null;
+			this.m_E_WatchButton = null;
+			this.m_E_WatchImage = null;
+			this.m_E_ChatButton = null;
+			this.m_E_ChatImage = null;
+			this.m_E_DeleteButton = null;
+			this.m_E_DeleteImage = null;
+			this.m_E_OccNameText = null;
 			this.uiTransform = null;
 			this.DataId = 0;
 		}
 
+		private UnityEngine.UI.Image m_E_HeadIconImage = null;
+		private UnityEngine.UI.Text m_E_PlayerNameText = null;
+		private UnityEngine.UI.Text m_E_PlayerLevelText = null;
+		private UnityEngine.UI.Text m_E_OnLineTimeText = null;
+		private UnityEngine.UI.Button m_E_WatchButton = null;
+		private UnityEngine.UI.Image m_E_WatchImage = null;
+		private UnityEngine.UI.Button m_E_ChatButton = null;
+		private UnityEngine.UI.Image m_E_ChatImage = null;
+		private UnityEngine.UI.Button m_E_DeleteButton = null;
+		private UnityEngine.UI.Image m_E_DeleteImage = null;
+		private UnityEngine.UI.Text m_E_OccNameText = null;
 		public Transform uiTransform = null;
 	}
 }
