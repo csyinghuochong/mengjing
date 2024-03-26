50c423a438f91ddcfc440f915cb520120f3db7f8
tangchunguang <394266315@qq.com>
2024-03-26 10:24:57 +0800

    no message

--- Unity/Assets/Scripts/Model/Server/MengJing/Pet/PetComponentS.cs
+++ Unity/Assets/Scripts/Model/Server/MengJing/Pet/PetComponentS.cs
@@ -9,22 +9,22 @@ namespace ET.Server
         public int PetFubeRewardId { get; set; }
 
         public int PetShouHuActive { get; set; }
-        public List<long> TeamPetList  { get; set; } = default;      //宠物天梯
-        public List<long> PetFormations { get; set; }= default;    //宠物副本
-        public List<long> PetShouHuList { get; set; }= default;    //守护列表（0-14宠物id  15/16/17矿场ID）
-        public List<long> PetMingList { get; set; }= default;     //矿场队伍(15个宠物）
-        public List<long> PetMingPosition { get; set; }= default;   //矿场宠物位置(27个位置)
-        public List<int>  PetCangKuOpen { get; set; }= default; 
+        public List<long> TeamPetList  { get; set; } = new List<long>();      //宠物天梯
+        public List<long> PetFormations { get; set; }= new List<long>();    //宠物副本
+        public List<long> PetShouHuList { get; set; }= new List<long>();    //守护列表（0-14宠物id  15/16/17矿场ID）
+        public List<long> PetMingList { get; set; }= new List<long>();     //矿场队伍(15个宠物）
+        public List<long> PetMingPosition { get; set; }= new List<long>();   //矿场宠物位置(27个位置)
+        public List<int>  PetCangKuOpen { get; set; }= new List<int>(); 
 
-        public List<RolePetInfo> RolePetInfos  { get; set; }= default; 
-        public List<KeyValuePairInt> RolePetEggs { get; set; }= default; 
-        public List<PetFubenInfo> PetFubenInfos{ get; set; }= default; 
-        public List<KeyValuePair> PetSkinList { get; set; }= default; 
+        public List<RolePetInfo> RolePetInfos  { get; set; }= new List<RolePetInfo>(); 
+        public List<KeyValuePairInt> RolePetEggs { get; set; }= new List<KeyValuePairInt>(); 
+        public List<PetFubenInfo> PetFubenInfos{ get; set; }= new List<PetFubenInfo>(); 
+        public List<KeyValuePair> PetSkinList { get; set; }= new List<KeyValuePair>(); 
 
-        public List<PetMingRecord> PetMingRecordList { get; set; }= default; 
+        public List<PetMingRecord> PetMingRecordList { get; set; }= new List<PetMingRecord>(); 
 
         //背包宠物
-        public List<RolePetInfo> RolePetBag { get; set; }= default; 
+        public List<RolePetInfo> RolePetBag { get; set; }= new List<RolePetInfo>(); 
 
         public int UpdateNumber { get; set; }  //1处理神兽技能
     }
