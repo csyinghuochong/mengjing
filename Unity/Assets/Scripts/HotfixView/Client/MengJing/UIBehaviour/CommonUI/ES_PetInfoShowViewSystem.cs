a4907d9e27257325d5708d7a92b29ea878c17884
FortuneCat0v0 <1142094319@qq.com>
2024-04-01 17:51:35 +0800

    (＠_＠;)

---
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIBehaviour/CommonUI/ES_PetInfoShowViewSystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET.Client
+{
+    [EntitySystemOf(typeof (ES_PetInfoShow))]
+    [FriendOfAttribute(typeof (ES_PetInfoShow))]
+    public static partial class ES_PetInfoShowSystem
+    {
+        [EntitySystem]
+        private static void Awake(this ES_PetInfoShow self, Transform transform)
+        {
+            self.uiTransform = transform;
+        }
+
+        [EntitySystem]
+        private static void Destroy(this ES_PetInfoShow self)
+        {
+            self.DestroyWidget();
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/CommonUI/ES_PetInfoShow.cs
@@ -0,0 +1,371 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET.Client
+{
+	[ChildOf]
+	[EnableMethod]
+	public  class ES_PetInfoShow : Entity,ET.IAwake<UnityEngine.Transform>,IDestroy 
+	{
+		public UnityEngine.RectTransform EG_ImageStarListRectTransform
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_EG_ImageStarListRectTransform == null )
+     			{
+		    		this.m_EG_ImageStarListRectTransform = UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"Left/EG_ImageStarList");
+     			}
+     			return this.m_EG_ImageStarListRectTransform;
+     		}
+     	}
+
+		public UnityEngine.RectTransform EG_PetZiZhiItem1RectTransform
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_EG_PetZiZhiItem1RectTransform == null )
+     			{
+		    		this.m_EG_PetZiZhiItem1RectTransform = UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"Left/EG_PetZiZhiItem1");
+     			}
+     			return this.m_EG_PetZiZhiItem1RectTransform;
+     		}
+     	}
+
+		public UnityEngine.RectTransform EG_PetZiZhiItem2RectTransform
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_EG_PetZiZhiItem2RectTransform == null )
+     			{
+		    		this.m_EG_PetZiZhiItem2RectTransform = UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"Left/EG_PetZiZhiItem2");
+     			}
+     			return this.m_EG_PetZiZhiItem2RectTransform;
+     		}
+     	}
+
+		public UnityEngine.RectTransform EG_PetZiZhiItem3RectTransform
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_EG_PetZiZhiItem3RectTransform == null )
+     			{
+		    		this.m_EG_PetZiZhiItem3RectTransform = UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"Left/EG_PetZiZhiItem3");
+     			}
+     			return this.m_EG_PetZiZhiItem3RectTransform;
+     		}
+     	}
+
+		public UnityEngine.RectTransform EG_PetZiZhiItem4RectTransform
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_EG_PetZiZhiItem4RectTransform == null )
+     			{
+		    		this.m_EG_PetZiZhiItem4RectTransform = UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"Left/EG_PetZiZhiItem4");
+     			}
+     			return this.m_EG_PetZiZhiItem4RectTransform;
+     		}
+     	}
+
+		public UnityEngine.RectTransform EG_PetZiZhiItem5RectTransform
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_EG_PetZiZhiItem5RectTransform == null )
+     			{
+		    		this.m_EG_PetZiZhiItem5RectTransform = UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"Left/EG_PetZiZhiItem5");
+     			}
+     			return this.m_EG_PetZiZhiItem5RectTransform;
+     		}
+     	}
+
+		public UnityEngine.RectTransform EG_PetZiZhiItem6RectTransform
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_EG_PetZiZhiItem6RectTransform == null )
+     			{
+		    		this.m_EG_PetZiZhiItem6RectTransform = UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"Left/EG_PetZiZhiItem6");
+     			}
+     			return this.m_EG_PetZiZhiItem6RectTransform;
+     		}
+     	}
+
+		public UnityEngine.UI.LoopVerticalScrollRect E_CommonSkillItemsLoopVerticalScrollRect
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_CommonSkillItemsLoopVerticalScrollRect == null )
+     			{
+		    		this.m_E_CommonSkillItemsLoopVerticalScrollRect = UIFindHelper.FindDeepChild<UnityEngine.UI.LoopVerticalScrollRect>(this.uiTransform.gameObject,"Left/E_CommonSkillItems");
+     			}
+     			return this.m_E_CommonSkillItemsLoopVerticalScrollRect;
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_Btn_QieHuanButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_Btn_QieHuanButton == null )
+     			{
+		    		this.m_E_Btn_QieHuanButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"Left/E_Btn_QieHuan");
+     			}
+     			return this.m_E_Btn_QieHuanButton;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_Btn_QieHuanImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_Btn_QieHuanImage == null )
+     			{
+		    		this.m_E_Btn_QieHuanImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"Left/E_Btn_QieHuan");
+     			}
+     			return this.m_E_Btn_QieHuanImage;
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_Img_PeteroQualityButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_Img_PeteroQualityButton == null )
+     			{
+		    		this.m_E_Img_PeteroQualityButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"Left/E_Img_PeteroQuality");
+     			}
+     			return this.m_E_Img_PeteroQualityButton;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_Img_PeteroQualityImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_Img_PeteroQualityImage == null )
+     			{
+		    		this.m_E_Img_PeteroQualityImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"Left/E_Img_PeteroQuality");
+     			}
+     			return this.m_E_Img_PeteroQualityImage;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_Img_PeteroQualityaddImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_Img_PeteroQualityaddImage == null )
+     			{
+		    		this.m_E_Img_PeteroQualityaddImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"Left/E_Img_PeteroQuality/E_Img_PeteroQualityadd");
+     			}
+     			return this.m_E_Img_PeteroQualityaddImage;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_Img_PetHeroIonImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_Img_PetHeroIonImage == null )
+     			{
+		    		this.m_E_Img_PetHeroIonImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"Left/E_Img_PetHeroIon");
+     			}
+     			return this.m_E_Img_PetHeroIonImage;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_ImageExpValueImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_ImageExpValueImage == null )
+     			{
+		    		this.m_E_ImageExpValueImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"Left/ImageExpDi/E_ImageExpValue");
+     			}
+     			return this.m_E_ImageExpValueImage;
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_Text_PetNameText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_Text_PetNameText == null )
+     			{
+		    		this.m_E_Text_PetNameText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"Left/E_Text_PetName");
+     			}
+     			return this.m_E_Text_PetNameText;
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_Text_PetLevelText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_Text_PetLevelText == null )
+     			{
+		    		this.m_E_Text_PetLevelText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"Left/E_Text_PetLevel");
+     			}
+     			return this.m_E_Text_PetLevelText;
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_Text_PetExpText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_Text_PetExpText == null )
+     			{
+		    		this.m_E_Text_PetExpText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"Left/E_Text_PetExp");
+     			}
+     			return this.m_E_Text_PetExpText;
+     		}
+     	}
+
+		    public Transform UITransform
+         {
+     	    get
+     	    {
+     		    return this.uiTransform;
+     	    }
+     	    set
+     	    {
+     		    this.uiTransform = value;
+     	    }
+         }
+
+		public void DestroyWidget()
+		{
+			this.m_EG_ImageStarListRectTransform = null;
+			this.m_EG_PetZiZhiItem1RectTransform = null;
+			this.m_EG_PetZiZhiItem2RectTransform = null;
+			this.m_EG_PetZiZhiItem3RectTransform = null;
+			this.m_EG_PetZiZhiItem4RectTransform = null;
+			this.m_EG_PetZiZhiItem5RectTransform = null;
+			this.m_EG_PetZiZhiItem6RectTransform = null;
+			this.m_E_CommonSkillItemsLoopVerticalScrollRect = null;
+			this.m_E_Btn_QieHuanButton = null;
+			this.m_E_Btn_QieHuanImage = null;
+			this.m_E_Img_PeteroQualityButton = null;
+			this.m_E_Img_PeteroQualityImage = null;
+			this.m_E_Img_PeteroQualityaddImage = null;
+			this.m_E_Img_PetHeroIonImage = null;
+			this.m_E_ImageExpValueImage = null;
+			this.m_E_Text_PetNameText = null;
+			this.m_E_Text_PetLevelText = null;
+			this.m_E_Text_PetExpText = null;
+			this.uiTransform = null;
+		}
+
+		private UnityEngine.RectTransform m_EG_ImageStarListRectTransform = null;
+		private UnityEngine.RectTransform m_EG_PetZiZhiItem1RectTransform = null;
+		private UnityEngine.RectTransform m_EG_PetZiZhiItem2RectTransform = null;
+		private UnityEngine.RectTransform m_EG_PetZiZhiItem3RectTransform = null;
+		private UnityEngine.RectTransform m_EG_PetZiZhiItem4RectTransform = null;
+		private UnityEngine.RectTransform m_EG_PetZiZhiItem5RectTransform = null;
+		private UnityEngine.RectTransform m_EG_PetZiZhiItem6RectTransform = null;
+		private UnityEngine.UI.LoopVerticalScrollRect m_E_CommonSkillItemsLoopVerticalScrollRect = null;
+		private UnityEngine.UI.Button m_E_Btn_QieHuanButton = null;
+		private UnityEngine.UI.Image m_E_Btn_QieHuanImage = null;
+		private UnityEngine.UI.Button m_E_Img_PeteroQualityButton = null;
+		private UnityEngine.UI.Image m_E_Img_PeteroQualityImage = null;
+		private UnityEngine.UI.Image m_E_Img_PeteroQualityaddImage = null;
+		private UnityEngine.UI.Image m_E_Img_PetHeroIonImage = null;
+		private UnityEngine.UI.Image m_E_ImageExpValueImage = null;
+		private UnityEngine.UI.Text m_E_Text_PetNameText = null;
+		private UnityEngine.UI.Text m_E_Text_PetLevelText = null;
+		private UnityEngine.UI.Text m_E_Text_PetExpText = null;
+		public Transform uiTransform = null;
+	}
+}
