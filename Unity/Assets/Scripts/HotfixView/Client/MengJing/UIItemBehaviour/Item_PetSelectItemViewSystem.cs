c110171ffc82115d9c37b1e426623adcd18a4672
FortuneCat0v0 <1142094319@qq.com>
2024-04-01 19:40:32 +0800

    (＠_＠;)

---
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_PetSelectItemViewSystem.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET.Client
+{
+	[EntitySystemOf(typeof(Scroll_Item_PetSelectItem))]
+	public static partial class Scroll_Item_PetSelectItemSystem 
+	{
+		[EntitySystem]
+		private static void Awake(this Scroll_Item_PetSelectItem self )
+		{
+		}
+
+		[EntitySystem]
+		private static void Destroy(this Scroll_Item_PetSelectItem self )
+		{
+			self.DestroyWidget();
+		}
+	}
+}
--- Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/DlgPetSelect/DlgPetSelectViewComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/DlgPetSelect/DlgPetSelectViewComponent.cs
@@ -7,11 +7,68 @@ namespace ET.Client
 	[EnableMethod]
 	public  class DlgPetSelectViewComponent : Entity,IAwake,IDestroy 
 	{
+		public UnityEngine.UI.Button E_Btn_CloseButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_Btn_CloseButton == null )
+     			{
+		    		this.m_E_Btn_CloseButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_Btn_Close");
+     			}
+     			return this.m_E_Btn_CloseButton;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_Btn_CloseImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_Btn_CloseImage == null )
+     			{
+		    		this.m_E_Btn_CloseImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_Btn_Close");
+     			}
+     			return this.m_E_Btn_CloseImage;
+     		}
+     	}
+
+		public UnityEngine.UI.LoopVerticalScrollRect E_PetSelectItemsLoopVerticalScrollRect
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_PetSelectItemsLoopVerticalScrollRect == null )
+     			{
+		    		this.m_E_PetSelectItemsLoopVerticalScrollRect = UIFindHelper.FindDeepChild<UnityEngine.UI.LoopVerticalScrollRect>(this.uiTransform.gameObject,"E_PetSelectItems");
+     			}
+     			return this.m_E_PetSelectItemsLoopVerticalScrollRect;
+     		}
+     	}
+
 		public void DestroyWidget()
 		{
+			this.m_E_Btn_CloseButton = null;
+			this.m_E_Btn_CloseImage = null;
+			this.m_E_PetSelectItemsLoopVerticalScrollRect = null;
 			this.uiTransform = null;
 		}
 
+		private UnityEngine.UI.Button m_E_Btn_CloseButton = null;
+		private UnityEngine.UI.Image m_E_Btn_CloseImage = null;
+		private UnityEngine.UI.LoopVerticalScrollRect m_E_PetSelectItemsLoopVerticalScrollRect = null;
 		public Transform uiTransform = null;
 	}
 }
