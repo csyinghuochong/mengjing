2b7a2713b0a2e18d432b145e61300aa791199e9e
tangchunguang <394266315@qq.com>
2024-04-03 17:20:56 +0800

    no message

--- Unity/Assets/Scripts/Model/Server/MengJing/Buff/BuffManagerComponentS.cs
+++ Unity/Assets/Scripts/Model/Server/MengJing/Buff/BuffManagerComponentS.cs
@@ -10,9 +10,10 @@ namespace ET.Server
         public int SceneType;
         
         //public List<BuffHandler> m_Buffs = new List<BuffHandler>();
+        
         public List<KeyValuePairLong> m_BuffRecord = new List<KeyValuePairLong>();  //buffid_增删_
-        //public readonly M2C_UnitBuffUpdate m2C_UnitBuffUpdate = new M2C_UnitBuffUpdate();
-        //public readonly M2C_UnitBuffRemove m2C_UnitBuffRemove = new M2C_UnitBuffRemove();   
+        public readonly M2C_UnitBuffUpdate m2C_UnitBuffUpdate = new M2C_UnitBuffUpdate();
+        public readonly M2C_UnitBuffRemove m2C_UnitBuffRemove = new M2C_UnitBuffRemove();   
     }
 }
 
