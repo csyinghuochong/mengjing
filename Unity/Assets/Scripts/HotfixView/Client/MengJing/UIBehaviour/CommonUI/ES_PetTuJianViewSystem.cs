d6cbb8113cfe3c25be1c72c75a2df7738b212474
FortuneCat0v0 <1142094319@qq.com>
2024-05-20 11:51:21 +0800

    ( •̀ .̫ •́ )✧

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UIBehaviour/CommonUI/ES_PetTuJianViewSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIBehaviour/CommonUI/ES_PetTuJianViewSystem.cs
@@ -111,7 +111,9 @@ namespace ET.Client
             self.UpdatePetSkinList(petid);
 
             PetConfig petConfig = PetConfigCategory.Instance.Get(petid);
-            self.ES_ModelShow.ShowOtherModel("Pet/" + petConfig.PetModel, true).Coroutine();
+            // self.ES_ModelShow.ShowOtherModel("Pet/" + petConfig.PetModel, true).Coroutine();
+            // 测试
+            self.ES_ModelShow.ShowOtherModel("Pet/1000101", true).Coroutine();
 
             self.E_Text_PetNameText.text = petConfig.PetName;
         }
@@ -135,7 +137,7 @@ namespace ET.Client
                 self.PetZiZhiItemList[5].transform.Find("Text_ZiZhiValue").GetComponent<Text>().text =
                         string.Format("{0}/{1}", (float)petConfig.ZiZhi_ChengZhang_Max, 1.25f);
                 Sprite sprite17 = self.Root().GetComponent<ResourcesLoaderComponent>()
-                        .LoadAssetSync<Sprite>("Assets/Bundles/Icon/OtherIcon/Img_20.png");
+                        .LoadAssetSync<Sprite>("Assets/Bundles/Icon/OtherIcon/Pro_17.png");
                 self.PetZiZhiItemList[0].transform.Find("ImageExpValue").GetComponent<Image>().sprite = sprite17;
                 self.PetZiZhiItemList[0].transform.Find("ImageExpValue").GetComponent<Image>().fillAmount =
                         Mathf.Clamp((float)petConfig.ZiZhi_Hp_Max / 3000f, 0f, 1f);
