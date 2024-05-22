ed74accaa4ad07514a8bd07f482a5ca84cfe041e
tangchunguang <394266315@qq.com>
2024-05-22 15:17:18 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/Union/Handler/C2U_UnionSignUpHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Union/Handler/C2U_UnionSignUpHandler.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ET
+namespace ET.Server
 {
-    [ActorMessageHandler]
-    public class C2U_UnionSignUpHandler : AMActorRpcHandler<Scene, C2U_UnionSignUpRequest, U2C_UnionSignUpResponse>
+    [MessageHandler(SceneType.Union)]
+    public class C2U_UnionSignUpHandler : MessageHandler<Scene, C2U_UnionSignUpRequest, U2C_UnionSignUpResponse>
     {
         protected override async ETTask Run(Scene scene, C2U_UnionSignUpRequest request, U2C_UnionSignUpResponse response, Action reply)
         {
