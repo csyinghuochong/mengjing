f8a4638539fae1e30e732f34c3ad83346d87ccd9
tangchunguang <394266315@qq.com>
2024-05-22 14:22:14 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/Union/Handler/C2U_UnionApplyReplyHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Union/Handler/C2U_UnionApplyReplyHandler.cs
@@ -7,13 +7,12 @@ namespace ET.Server
     public class C2U_UnionApplyReplyHandler : MessageHandler<Scene, C2U_UnionApplyReplyRequest, U2C_UnionApplyReplyResponse>
     {
 
-        protected override async ETTask Run(Scene scene, C2U_UnionApplyReplyRequest request, U2C_UnionApplyReplyResponse response, Action reply)
+        protected override async ETTask Run(Scene scene, C2U_UnionApplyReplyRequest request, U2C_UnionApplyReplyResponse response)
         {
-            using (await CoroutineLockComponent.Instance.Wait(CoroutineLockType.UnionOperate, request.UnionId)) 
+            using (await scene.Root().GetComponent<CoroutineLockComponent>().Wait(CoroutineLockType.UnionOperate, request.UnionId)) 
             {
                 response.Error = await scene.GetComponent<UnionSceneComponent>().OnJoinUinon(request.UnionId, request.UserId, request.ReplyCode);
             }
-            reply();
         }
     }
 }
