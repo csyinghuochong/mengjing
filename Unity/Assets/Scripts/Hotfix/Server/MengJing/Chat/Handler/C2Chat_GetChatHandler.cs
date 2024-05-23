dc4b3fa1996e5bb3a859f6a0e3aa814b7ba1dcf4
tangchunguang <394266315@qq.com>
2024-05-23 20:33:49 +0800

    no message

---
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Chat/Handler/C2Chat_GetChatHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ET
+{
+
+    [ActorMessageHandler]
+    public class C2Chat_GetChatHandler : AMActorRpcHandler<ChatInfoUnit, C2Chat_GetChatRequest, Chat2C_GetChatResponse>
+    {
+        protected override async ETTask Run(ChatInfoUnit chatInfoUnit, C2Chat_GetChatRequest request, Chat2C_GetChatResponse response, Action reply)
+        {
+            long serverTime = TimeHelper.ServerNow();
+            ChatSceneComponent chatInfoUnitsComponent = chatInfoUnit.DomainScene().GetComponent<ChatSceneComponent>();
+
+            for (int i = 0; i < chatInfoUnitsComponent.WordChatInfos.Count; i++)
+            {
+                ChatInfo chatInfo = chatInfoUnitsComponent.WordChatInfos[i];
+                if (serverTime - chatInfo.Time < TimeHelper.OneDay)
+                {
+                    response.ChatInfos.Add(chatInfo);
+                }
+            }
+            
+
+            reply();
+            await ETTask.CompletedTask;
+        }
+    }
+}
