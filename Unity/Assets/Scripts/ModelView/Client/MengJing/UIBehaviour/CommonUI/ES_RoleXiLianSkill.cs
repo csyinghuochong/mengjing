60261a8d43439dbfe158e95601020395a3bb678b
FortuneCat0v0 <1142094319@qq.com>
2024-05-28 13:20:05 +0800

    .

--- Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/CommonUI/ES_RoleXiLianSkill.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/CommonUI/ES_RoleXiLianSkill.cs
@@ -29,57 +29,6 @@ namespace ET.Client
      		}
      	}
 
-		public UnityEngine.UI.Text E_Text_XiLianNameText
-     	{
-     		get
-     		{
-     			if (this.uiTransform == null)
-     			{
-     				Log.Error("uiTransform is null.");
-     				return null;
-     			}
-     			if( this.m_E_Text_XiLianNameText == null )
-     			{
-		    		this.m_E_Text_XiLianNameText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_RoleXiLianSkillItems/Content/Item_RoleXiLianSkillItem/TopNode/E_Text_XiLianName");
-     			}
-     			return this.m_E_Text_XiLianNameText;
-     		}
-     	}
-
-		public UnityEngine.RectTransform EG_JiHuoSetRectTransform
-     	{
-     		get
-     		{
-     			if (this.uiTransform == null)
-     			{
-     				Log.Error("uiTransform is null.");
-     				return null;
-     			}
-     			if( this.m_EG_JiHuoSetRectTransform == null )
-     			{
-		    		this.m_EG_JiHuoSetRectTransform = UIFindHelper.FindDeepChild<UnityEngine.RectTransform>(this.uiTransform.gameObject,"E_RoleXiLianSkillItems/Content/Item_RoleXiLianSkillItem/TopNode/EG_JiHuoSet");
-     			}
-     			return this.m_EG_JiHuoSetRectTransform;
-     		}
-     	}
-
-		public UnityEngine.UI.LoopVerticalScrollRect E_CommonSkillItemsLoopVerticalScrollRect
-     	{
-     		get
-     		{
-     			if (this.uiTransform == null)
-     			{
-     				Log.Error("uiTransform is null.");
-     				return null;
-     			}
-     			if( this.m_E_CommonSkillItemsLoopVerticalScrollRect == null )
-     			{
-		    		this.m_E_CommonSkillItemsLoopVerticalScrollRect = UIFindHelper.FindDeepChild<UnityEngine.UI.LoopVerticalScrollRect>(this.uiTransform.gameObject,"E_RoleXiLianSkillItems/Content/Item_RoleXiLianSkillItem/E_CommonSkillItems");
-     			}
-     			return this.m_E_CommonSkillItemsLoopVerticalScrollRect;
-     		}
-     	}
-
 		    public Transform UITransform
          {
      	    get
@@ -95,16 +44,10 @@ namespace ET.Client
 		public void DestroyWidget()
 		{
 			this.m_E_RoleXiLianSkillItemsLoopVerticalScrollRect = null;
-			this.m_E_Text_XiLianNameText = null;
-			this.m_EG_JiHuoSetRectTransform = null;
-			this.m_E_CommonSkillItemsLoopVerticalScrollRect = null;
 			this.uiTransform = null;
 		}
 
 		private UnityEngine.UI.LoopVerticalScrollRect m_E_RoleXiLianSkillItemsLoopVerticalScrollRect = null;
-		private UnityEngine.UI.Text m_E_Text_XiLianNameText = null;
-		private UnityEngine.RectTransform m_EG_JiHuoSetRectTransform = null;
-		private UnityEngine.UI.LoopVerticalScrollRect m_E_CommonSkillItemsLoopVerticalScrollRect = null;
 		public Transform uiTransform = null;
 	}
 }
