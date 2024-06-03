419cbef95e004dfb55a7f82dd3537fb4d6d9d347
FortuneCat0v0 <1142094319@qq.com>
2024-06-03 16:00:11 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_CommonCostItemViewSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_CommonCostItemViewSystem.cs
@@ -1,43 +1,56 @@
-﻿
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace ET.Client
 {
-	[EntitySystemOf(typeof (Scroll_Item_CommonCostItem))]
-	public static partial class Scroll_Item_CommonCostItemSystem
-	{
-		[EntitySystem]
-		private static void Awake(this Scroll_Item_CommonCostItem self)
-		{
-		}
+    [EntitySystemOf(typeof (Scroll_Item_CommonCostItem))]
+    public static partial class Scroll_Item_CommonCostItemSystem
+    {
+        [EntitySystem]
+        private static void Awake(this Scroll_Item_CommonCostItem self)
+        {
+        }
 
-		[EntitySystem]
-		private static void Destroy(this Scroll_Item_CommonCostItem self)
-		{
-			self.DestroyWidget();
-		}
+        [EntitySystem]
+        private static void Destroy(this Scroll_Item_CommonCostItem self)
+        {
+            self.DestroyWidget();
+        }
 
-		public static void UpdateItem(this Scroll_Item_CommonCostItem self, int itemId, int itemNum)
-		{
-			BagComponentC bagComponent = self.Root().GetComponent<BagComponentC>();
-			ItemConfig itemConfig = ItemConfigCategory.Instance.Get(itemId);
+        public static void UpdateItem(this Scroll_Item_CommonCostItem self, int itemId, int itemNum)
+        {
+            BagComponentC bagComponent = self.Root().GetComponent<BagComponentC>();
+            ItemConfig itemConfig = ItemConfigCategory.Instance.Get(itemId);
 
-			self.E_ItemNameText.text = itemConfig.ItemName;
+            self.E_ItemNameText.text = itemConfig.ItemName;
 
-			//显示字
-			//self.Label_ItemNum.GetComponent<Text>().text = $"{bagComponent.GetItemNumber(itemId)}/{itemNum}";
-			self.E_ItemNumText.text = $"{UICommonHelper.NumToWString(bagComponent.GetItemNumber(itemId))}/{UICommonHelper.NumToWString(itemNum)}";
-			//显示颜色
-			self.E_ItemNumText.color = (itemNum <= bagComponent.GetItemNumber(itemId))? Color.green : Color.red;
-			string path = ABPathHelper.GetAtlasPath_2(ABAtlasTypes.ItemIcon, itemConfig.Icon);
-			Sprite sp = self.Root().GetComponent<ResourcesLoaderComponent>().LoadAssetSync<Sprite>(path);
-			self.E_ItemIconImage.sprite = sp;
+            //显示字
+            //self.Label_ItemNum.GetComponent<Text>().text = $"{bagComponent.GetItemNumber(itemId)}/{itemNum}";
+            self.E_ItemNumText.text = $"{UICommonHelper.NumToWString(bagComponent.GetItemNumber(itemId))}/{UICommonHelper.NumToWString(itemNum)}";
+            //显示颜色
+            self.E_ItemNumText.color = (itemNum <= bagComponent.GetItemNumber(itemId))? Color.green : Color.red;
+            string path = ABPathHelper.GetAtlasPath_2(ABAtlasTypes.ItemIcon, itemConfig.Icon);
+            Sprite sp = self.Root().GetComponent<ResourcesLoaderComponent>().LoadAssetSync<Sprite>(path);
+            self.E_ItemIconImage.sprite = sp;
 
-			string qualityiconStr = FunctionUI.ItemQualiytoPath(itemConfig.ItemQuality);
-			string path2 = ABPathHelper.GetAtlasPath_2(ABAtlasTypes.ItemQualityIcon, qualityiconStr);
-			Sprite sp2 = self.Root().GetComponent<ResourcesLoaderComponent>().LoadAssetSync<Sprite>(path2);
-			self.E_ItemQualityImage.sprite = sp2;
-		}
-	}
+            string qualityiconStr = FunctionUI.ItemQualiytoPath(itemConfig.ItemQuality);
+            string path2 = ABPathHelper.GetAtlasPath_2(ABAtlasTypes.ItemQualityIcon, qualityiconStr);
+            Sprite sp2 = self.Root().GetComponent<ResourcesLoaderComponent>().LoadAssetSync<Sprite>(path2);
+            self.E_ItemQualityImage.sprite = sp2;
+
+            self.E_ItemButtonButton.AddListener(() =>
+            {
+                EventSystem.Instance.Publish(self.Root(),
+                    new ShowItemTips()
+                    {
+                        BagInfo = new BagInfo() { ItemID = itemId, ItemNum = itemNum },
+                        ItemOperateEnum = ItemOperateEnum.None,
+                        InputPoint = Input.mousePosition,
+                        Occ = self.Root().GetComponent<UserInfoComponentC>().UserInfo.Occ,
+                        EquipList = new List<BagInfo>()
+                    });
+            });
+        }
+    }
 }
