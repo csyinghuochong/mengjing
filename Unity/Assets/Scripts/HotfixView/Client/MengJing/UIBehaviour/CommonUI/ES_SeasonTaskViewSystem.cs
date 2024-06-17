4dc3bae6e1288481bac657c2584cbd0929fe4254
FortuneCat0v0 <1142094319@qq.com>
2024-06-17 18:52:22 +0800

    .

---
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIBehaviour/CommonUI/ES_SeasonTaskViewSystem.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET.Client
+{
+	[EntitySystemOf(typeof(ES_SeasonTask))]
+	[FriendOfAttribute(typeof(ES_SeasonTask))]
+	public static partial class ES_SeasonTaskSystem 
+	{
+		[EntitySystem]
+		private static void Awake(this ES_SeasonTask self,Transform transform)
+		{
+			self.uiTransform = transform;
+		}
+
+		[EntitySystem]
+		private static void Destroy(this ES_SeasonTask self)
+		{
+			self.DestroyWidget();
+		}
+	}
+
+
+}
---
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/CommonUI/ES_SeasonTask.cs
@@ -0,0 +1,258 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET.Client
+{
+	[ChildOf]
+	[EnableMethod]
+	public  class ES_SeasonTask : Entity,ET.IAwake<UnityEngine.Transform>,IDestroy 
+	{
+		public UnityEngine.UI.ToggleGroup E_ItemTypeSetToggleGroup
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_ItemTypeSetToggleGroup == null )
+     			{
+		    		this.m_E_ItemTypeSetToggleGroup = UIFindHelper.FindDeepChild<UnityEngine.UI.ToggleGroup>(this.uiTransform.gameObject,"E_ItemTypeSet");
+     			}
+     			return this.m_E_ItemTypeSetToggleGroup;
+     		}
+     	}
+
+		public UnityEngine.UI.Toggle E_ItemTypeAllSetToggle
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_ItemTypeAllSetToggle == null )
+     			{
+		    		this.m_E_ItemTypeAllSetToggle = UIFindHelper.FindDeepChild<UnityEngine.UI.Toggle>(this.uiTransform.gameObject,"E_ItemTypeSet/E_ItemTypeAllSet");
+     			}
+     			return this.m_E_ItemTypeAllSetToggle;
+     		}
+     	}
+
+		public UnityEngine.UI.Toggle E_ItemTypeEquipSetToggle
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_ItemTypeEquipSetToggle == null )
+     			{
+		    		this.m_E_ItemTypeEquipSetToggle = UIFindHelper.FindDeepChild<UnityEngine.UI.Toggle>(this.uiTransform.gameObject,"E_ItemTypeSet/E_ItemTypeEquipSet");
+     			}
+     			return this.m_E_ItemTypeEquipSetToggle;
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_TaskNameTextText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_TaskNameTextText == null )
+     			{
+		    		this.m_E_TaskNameTextText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_TaskNameText");
+     			}
+     			return this.m_E_TaskNameTextText;
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_ProgressTextText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_ProgressTextText == null )
+     			{
+		    		this.m_E_ProgressTextText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_ProgressText");
+     			}
+     			return this.m_E_ProgressTextText;
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_TaskDescTextText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_TaskDescTextText == null )
+     			{
+		    		this.m_E_TaskDescTextText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_TaskDescText");
+     			}
+     			return this.m_E_TaskDescTextText;
+     		}
+     	}
+
+		public ES_RewardList ES_RewardList
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_es_rewardlist == null )
+     			{
+		    	   Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"ES_RewardList");
+		    	   this.m_es_rewardlist = this.AddChild<ES_RewardList,Transform>(subTrans);
+     			}
+     			return this.m_es_rewardlist;
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_GetBtnButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_GetBtnButton == null )
+     			{
+		    		this.m_E_GetBtnButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_GetBtn");
+     			}
+     			return this.m_E_GetBtnButton;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_GetBtnImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_GetBtnImage == null )
+     			{
+		    		this.m_E_GetBtnImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_GetBtn");
+     			}
+     			return this.m_E_GetBtnImage;
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_GiveBtnButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_GiveBtnButton == null )
+     			{
+		    		this.m_E_GiveBtnButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_GiveBtn");
+     			}
+     			return this.m_E_GiveBtnButton;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_GiveBtnImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_GiveBtnImage == null )
+     			{
+		    		this.m_E_GiveBtnImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_GiveBtn");
+     			}
+     			return this.m_E_GiveBtnImage;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_AcvityedImgImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_AcvityedImgImage == null )
+     			{
+		    		this.m_E_AcvityedImgImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_AcvityedImg");
+     			}
+     			return this.m_E_AcvityedImgImage;
+     		}
+     	}
+
+		    public Transform UITransform
+         {
+     	    get
+     	    {
+     		    return this.uiTransform;
+     	    }
+     	    set
+     	    {
+     		    this.uiTransform = value;
+     	    }
+         }
+
+		public void DestroyWidget()
+		{
+			this.m_E_ItemTypeSetToggleGroup = null;
+			this.m_E_ItemTypeAllSetToggle = null;
+			this.m_E_ItemTypeEquipSetToggle = null;
+			this.m_E_TaskNameTextText = null;
+			this.m_E_ProgressTextText = null;
+			this.m_E_TaskDescTextText = null;
+			this.m_es_rewardlist = null;
+			this.m_E_GetBtnButton = null;
+			this.m_E_GetBtnImage = null;
+			this.m_E_GiveBtnButton = null;
+			this.m_E_GiveBtnImage = null;
+			this.m_E_AcvityedImgImage = null;
+			this.uiTransform = null;
+		}
+
+		private UnityEngine.UI.ToggleGroup m_E_ItemTypeSetToggleGroup = null;
+		private UnityEngine.UI.Toggle m_E_ItemTypeAllSetToggle = null;
+		private UnityEngine.UI.Toggle m_E_ItemTypeEquipSetToggle = null;
+		private UnityEngine.UI.Text m_E_TaskNameTextText = null;
+		private UnityEngine.UI.Text m_E_ProgressTextText = null;
+		private UnityEngine.UI.Text m_E_TaskDescTextText = null;
+		private EntityRef<ES_RewardList> m_es_rewardlist = null;
+		private UnityEngine.UI.Button m_E_GetBtnButton = null;
+		private UnityEngine.UI.Image m_E_GetBtnImage = null;
+		private UnityEngine.UI.Button m_E_GiveBtnButton = null;
+		private UnityEngine.UI.Image m_E_GiveBtnImage = null;
+		private UnityEngine.UI.Image m_E_AcvityedImgImage = null;
+		public Transform uiTransform = null;
+	}
+}
