a63b612e167d4ecd98af135fd14557bc64c1f920
tangchunguang <394266315@qq.com>
2024-06-26 17:37:18 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/Union/FiberInit_Union.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Union/FiberInit_Union.cs
@@ -14,7 +14,9 @@ namespace ET.Server
             root.AddComponent<ProcessInnerSender>();
             root.AddComponent<MessageSender>();
             root.AddComponent<LocationManagerComoponent>();
+            root.AddComponent<LocationProxyComponent>();
             root.AddComponent<DBManagerComponent>();
+            root.AddComponent<MessageLocationSenderComponent>();
             root.AddComponent<UnionSceneComponent>();
             
             await ETTask.CompletedTask;
