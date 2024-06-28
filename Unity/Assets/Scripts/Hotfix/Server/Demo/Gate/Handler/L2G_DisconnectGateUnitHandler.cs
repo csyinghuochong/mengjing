4d8a485c45b2145f754387eed4ac331a11037279
tangchunguang <394266315@qq.com>
2024-06-28 17:29:07 +0800

    no message

---
+++ Unity/Assets/Scripts/Hotfix/Server/Demo/Gate/Handler/L2G_DisconnectGateUnitHandler.cs
@@ -0,0 +1,39 @@
+namespace ET.Server
+{
+    [MessageHandler(SceneType.Gate)]
+    [FriendOfAttribute(typeof(ET.Server.Player))]
+    public class L2G_DisconnectGateUnitHandler : MessageHandler<Scene, L2G_DisconnectGateUnit, G2L_DisconnectGateUnit>
+    {
+        protected override async ETTask Run(Scene scene, L2G_DisconnectGateUnit request, G2L_DisconnectGateUnit response)
+        {
+            CoroutineLockComponent coroutineLockComponent = scene.GetComponent<CoroutineLockComponent>();
+            using (await coroutineLockComponent.Wait(CoroutineLockType.LoginGate, request.AccountName.GetLongHashCode()))
+            {
+                PlayerComponent playerComponent = scene.GetComponent<PlayerComponent>();
+                Player player = playerComponent.GetByAccount(request.AccountName);
+
+                if (player == null)
+                {
+                    return;
+                }
+
+                scene.GetComponent<GateSessionKeyComponent>().Remove(request.AccountName.GetLongHashCode());
+                
+                Session gateSession = player.GetComponent<PlayerSessionComponent>()?.Session;
+                if (gateSession != null && !gateSession.IsDisposed)
+                {
+                    A2C_Disconnect a2CDisconnect = A2C_Disconnect.Create();
+                    a2CDisconnect.Error = ErrorCode.ERR_OtherAccountLogin;
+                    gateSession.Send(a2CDisconnect);
+                    gateSession?.Disconnect().Coroutine();
+                }
+
+                if ( player.GetComponent<PlayerSessionComponent>()?.Session != null)
+                {
+                    player.GetComponent<PlayerSessionComponent>().Session = null;
+                }
+                player.AddComponent<PlayerOfflineOutTimeComponent>();
+            }
+        }
+    }
+}
