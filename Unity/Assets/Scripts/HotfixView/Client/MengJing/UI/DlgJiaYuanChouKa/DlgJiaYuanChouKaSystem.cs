afc3d3729da99e175c67a5420b0f1c06fd239690
FortuneCat0v0 <1142094319@qq.com>
2024-07-04 14:48:05 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgJiaYuanChouKa/DlgJiaYuanChouKaSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgJiaYuanChouKa/DlgJiaYuanChouKaSystem.cs
@@ -6,20 +6,28 @@ using UnityEngine.UI;
 
 namespace ET.Client
 {
-	[FriendOf(typeof(DlgJiaYuanChouKa))]
-	public static  class DlgJiaYuanChouKaSystem
-	{
+    [FriendOf(typeof (DlgJiaYuanChouKa))]
+    public static class DlgJiaYuanChouKaSystem
+    {
+        public static void RegisterUIEvent(this DlgJiaYuanChouKa self)
+        {
+            self.View.E_Btn_ChouKaOneButton.AddListener(() => self.ChouKa(1));
+            self.View.E_Btn_ChouKaTenButton.AddListener(() => self.ChouKa(10));
+        }
 
-		public static void RegisterUIEvent(this DlgJiaYuanChouKa self)
-		{
-		 
-		}
+        public static void ShowWindow(this DlgJiaYuanChouKa self, Entity contextData = null)
+        {
+        }
 
-		public static void ShowWindow(this DlgJiaYuanChouKa self, Entity contextData = null)
-		{
-		}
+        //抽卡
+        public static void ChouKa(this DlgJiaYuanChouKa self, int chouKaNum)
+        {
+        }
 
-		 
-
-	}
+        public static void ShowChouKaReward(this DlgJiaYuanChouKa self)
+        {
+            string rewardListStr = "10036001;1@10036035;1";
+            self.View.ES_RewardList.Refresh(rewardListStr, 0.8f);
+        }
+    }
 }
