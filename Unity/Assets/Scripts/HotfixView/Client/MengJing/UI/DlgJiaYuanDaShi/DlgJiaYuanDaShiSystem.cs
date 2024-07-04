a0323671f872ab8af89933a32f85a5e5ad5de9dc
FortuneCat0v0 <1142094319@qq.com>
2024-07-04 15:34:35 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgJiaYuanDaShi/DlgJiaYuanDaShiSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgJiaYuanDaShi/DlgJiaYuanDaShiSystem.cs
@@ -6,20 +6,29 @@ using UnityEngine.UI;
 
 namespace ET.Client
 {
-	[FriendOf(typeof(DlgJiaYuanDaShi))]
-	public static  class DlgJiaYuanDaShiSystem
-	{
+    [FriendOf(typeof (DlgJiaYuanDaShi))]
+    public static class DlgJiaYuanDaShiSystem
+    {
+        public static void RegisterUIEvent(this DlgJiaYuanDaShi self)
+        {
+            self.View.E_FunctionSetBtnToggleGroup.AddListener(self.OnFunctionSetBtn);
+        }
 
-		public static void RegisterUIEvent(this DlgJiaYuanDaShi self)
-		{
-		 
-		}
+        public static void ShowWindow(this DlgJiaYuanDaShi self, Entity contextData = null)
+        {
+            self.View.E_FunctionSetBtnToggleGroup.OnSelectIndex(0);
+        }
 
-		public static void ShowWindow(this DlgJiaYuanDaShi self, Entity contextData = null)
-		{
-		}
-
-		 
-
-	}
+        private static void OnFunctionSetBtn(this DlgJiaYuanDaShi self, int index)
+        {
+            CommonViewHelper.HideChildren(self.View.EG_SubViewRectTransform);
+            switch (index)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+            }
+        }
+    }
 }
