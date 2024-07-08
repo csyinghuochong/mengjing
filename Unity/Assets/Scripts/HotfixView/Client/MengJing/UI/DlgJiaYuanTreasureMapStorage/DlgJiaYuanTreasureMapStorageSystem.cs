a895947d4263e896ccdeef3150834848c27661ab
FortuneCat0v0 <1142094319@qq.com>
2024-07-08 18:41:37 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgJiaYuanTreasureMapStorage/DlgJiaYuanTreasureMapStorageSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgJiaYuanTreasureMapStorage/DlgJiaYuanTreasureMapStorageSystem.cs
@@ -6,20 +6,124 @@ using UnityEngine.UI;
 
 namespace ET.Client
 {
-	[FriendOf(typeof(DlgJiaYuanTreasureMapStorage))]
-	public static  class DlgJiaYuanTreasureMapStorageSystem
-	{
+    [Event(SceneType.Demo)]
+    public class BagItemUpdate_DlgJiaYuanTreasureMapStorageRefresh: AEvent<Scene, DataUpdate_BagItemUpdate>
+    {
+        protected override async ETTask Run(Scene scene, DataUpdate_BagItemUpdate args)
+        {
+            scene.GetComponent<UIComponent>().GetDlgLogic<DlgJiaYuanTreasureMapStorage>()?.OnUpdateUI();
+            await ETTask.CompletedTask;
+        }
+    }
 
-		public static void RegisterUIEvent(this DlgJiaYuanTreasureMapStorage self)
-		{
-		 
-		}
+    [FriendOf(typeof (DlgJiaYuanTreasureMapStorage))]
+    public static class DlgJiaYuanTreasureMapStorageSystem
+    {
+        public static void RegisterUIEvent(this DlgJiaYuanTreasureMapStorage self)
+        {
+            self.View.E_ItemTypeSetToggleGroup.AddListener(self.OnItemTypeSet);
+            self.View.E_TakeOutAllButton.AddListenerAsync(self.OnButtonTakeOutAll);
+            self.View.E_OneKeyButton.AddListenerAsync(self.OnButtonOneKey);
 
-		public static void ShowWindow(this DlgJiaYuanTreasureMapStorage self, Entity contextData = null)
-		{
-		}
+            self.View.E_BagItems1LoopVerticalScrollRect.AddItemRefreshListener(self.OnHouseItemsRefresh);
+            self.View.E_BagItems2LoopVerticalScrollRect.AddItemRefreshListener(self.OnBagItemsRefresh);
+        }
 
-		 
+        public static void ShowWindow(this DlgJiaYuanTreasureMapStorage self, Entity contextData = null)
+        {
+            self.View.E_ItemTypeSetToggleGroup.OnSelectIndex(0);
+        }
 
-	}
+        private static void OnItemTypeSet(this DlgJiaYuanTreasureMapStorage self, int index)
+        {
+            self.Root().GetComponent<BagComponentC>().CurrentHouse =
+                    self.View.E_ItemTypeSetToggleGroup.GetCurrentIndex() + (int)ItemLocType.JianYuanWareHouse1;
+            self.UpdateStorage();
+            self.UpdateBagList();
+        }
+
+        public static async ETTask OnButtonTakeOutAll(this DlgJiaYuanTreasureMapStorage self)
+        {
+            await BagClientNetHelper.RquestTakeOutAll(self.Root(), self.Root().GetComponent<BagComponentC>().CurrentHouse);
+        }
+
+        public static async ETTask OnButtonOneKey(this DlgJiaYuanTreasureMapStorage self)
+        {
+            await JiaYuanNetHelper.JiaYuanStoreRequest(self.Root(), self.Root().GetComponent<BagComponentC>().CurrentHouse);
+        }
+
+        private static void OnHouseItemsRefresh(this DlgJiaYuanTreasureMapStorage self, Transform transform, int index)
+        {
+            Scroll_Item_CommonItem scrollItemCommonItem = self.ScrollItemHouseItems[index].BindTrans(transform);
+
+            if (index < self.ShowHouseBagInfos.Count)
+            {
+                scrollItemCommonItem.Refresh(self.ShowHouseBagInfos[index], ItemOperateEnum.Cangku);
+            }
+            else
+            {
+                scrollItemCommonItem.Refresh(null, ItemOperateEnum.None);
+            }
+        }
+
+        public static void UpdateStorage(this DlgJiaYuanTreasureMapStorage self)
+        {
+            int curindex = self.View.E_ItemTypeSetToggleGroup.GetCurrentIndex();
+
+            int storageNumber = GlobalValueConfigCategory.Instance.HourseInitCapacity;
+            self.ShowHouseBagInfos = self.Root().GetComponent<BagComponentC>().GetItemsByLoc(curindex + ItemLocType.JianYuanTreasureMapStorage1);
+
+            self.AddUIScrollItems(ref self.ScrollItemHouseItems, storageNumber);
+            self.View.E_BagItems1LoopVerticalScrollRect.SetVisible(true, storageNumber);
+        }
+
+        private static void OnBagItemsRefresh(this DlgJiaYuanTreasureMapStorage self, Transform transform, int index)
+        {
+            Scroll_Item_CommonItem scrollItemCommonItem = self.ScrollItemBagItems[index].BindTrans(transform);
+
+            if (index < self.ShowBagBagInfos.Count)
+            {
+                scrollItemCommonItem.Refresh(self.ShowBagBagInfos[index], ItemOperateEnum.CangkuBag);
+            }
+            else
+            {
+                scrollItemCommonItem.Refresh(null, ItemOperateEnum.None);
+            }
+        }
+
+        public static void UpdateBagList(this DlgJiaYuanTreasureMapStorage self)
+        {
+            int bagcellNumber = self.Root().GetComponent<BagComponentC>().GetBagTotalCell();
+
+            List<BagInfo> bagInfos = self.Root().GetComponent<BagComponentC>().GetItemsByLoc(ItemLocType.ItemLocBag);
+
+            if (self.View.E_ItemTypeSetToggleGroup.GetCurrentIndex() == 0)
+            {
+                self.ShowBagBagInfos = ItemHelper.GetTreasureMapList(bagInfos);
+            }
+            else if (self.View.E_ItemTypeSetToggleGroup.GetCurrentIndex() == 1)
+            {
+                self.ShowBagBagInfos = ItemHelper.GetTreasureMapList2(bagInfos);
+            }
+
+            self.AddUIScrollItems(ref self.ScrollItemBagItems, bagcellNumber);
+            self.View.E_BagItems2LoopVerticalScrollRect.SetVisible(true, bagcellNumber);
+        }
+
+        public static void OnUpdateUI(this DlgJiaYuanTreasureMapStorage self)
+        {
+            if (self.ScrollItemHouseItems == null)
+            {
+                return;
+            }
+
+            if (self.ScrollItemHouseItems.Count < GlobalValueConfigCategory.Instance.HourseInitCapacity)
+            {
+                return;
+            }
+
+            self.UpdateStorage();
+            self.UpdateBagList();
+        }
+    }
 }
--- Unity/Assets/Scripts/ModelView/Client/MengJing/UI/DlgJiaYuanTreasureMapStorage/DlgJiaYuanTreasureMapStorage.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UI/DlgJiaYuanTreasureMapStorage/DlgJiaYuanTreasureMapStorage.cs
@@ -1,12 +1,18 @@
-﻿namespace ET.Client
+﻿using System.Collections.Generic;
+
+namespace ET.Client
 {
-	 [ComponentOf(typeof(UIBaseWindow))]
-	public  class DlgJiaYuanTreasureMapStorage :Entity,IAwake,IUILogic
-	{
+    [ComponentOf(typeof (UIBaseWindow))]
+    public class DlgJiaYuanTreasureMapStorage: Entity, IAwake, IUILogic
+    {
+        public DlgJiaYuanTreasureMapStorageViewComponent View
+        {
+            get => this.GetComponent<DlgJiaYuanTreasureMapStorageViewComponent>();
+        }
 
-		public DlgJiaYuanTreasureMapStorageViewComponent View { get => this.GetComponent<DlgJiaYuanTreasureMapStorageViewComponent>();} 
-
-		 
-
-	}
+        public Dictionary<int, Scroll_Item_CommonItem> ScrollItemHouseItems;
+        public List<BagInfo> ShowHouseBagInfos;
+        public Dictionary<int, Scroll_Item_CommonItem> ScrollItemBagItems;
+        public List<BagInfo> ShowBagBagInfos;
+    }
 }
