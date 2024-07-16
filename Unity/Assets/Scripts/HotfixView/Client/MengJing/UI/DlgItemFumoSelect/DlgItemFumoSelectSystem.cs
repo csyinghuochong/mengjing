f92de95bd3a916a342077c53fb804ae3c2747529
FortuneCat0v0 <1142094319@qq.com>
2024-07-16 14:01:59 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgItemFumoSelect/DlgItemFumoSelectSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgItemFumoSelect/DlgItemFumoSelectSystem.cs
@@ -6,81 +6,111 @@ using UnityEngine.UI;
 
 namespace ET.Client
 {
+    [FriendOf(typeof(ES_CommonItem))]
     [FriendOf(typeof(DlgItemFumoSelect))]
     public static class DlgItemFumoSelectSystem
     {
         public static void RegisterUIEvent(this DlgItemFumoSelect self)
         {
+            self.View.E_BtnCloseButton.AddListener(self.OnCloseTips);
         }
 
         public static void ShowWindow(this DlgItemFumoSelect self, Entity contextData = null)
         {
+            self.ItemList.Add(self.View.ES_CommonItem_0);
+            self.ItemList.Add(self.View.ES_CommonItem_1);
+            self.ItemList.Add(self.View.ES_CommonItem_2);
+
+            ES_CommonItem item = self.View.ES_CommonItem_0;
+            item.uiTransform.gameObject.SetActive(false);
+            item.SetClickHandler((BagInfo bagInfo) => { self.OnSetClickHandler(bagInfo).Coroutine(); });
+
+            item = self.View.ES_CommonItem_1;
+            item.uiTransform.gameObject.SetActive(false);
+            item.SetClickHandler((BagInfo bagInfo) => { self.OnSetClickHandler(bagInfo).Coroutine(); });
+
+            item = self.View.ES_CommonItem_2;
+            item.uiTransform.gameObject.SetActive(false);
+            item.SetClickHandler((BagInfo bagInfo) => { self.OnSetClickHandler(bagInfo).Coroutine(); });
+
+            Text text = self.View.E_Label_ItemFumo_0Text;
+            text.text = string.Empty;
+            self.TextFomoPro.Add(text);
+
+            text = self.View.E_Label_ItemFumo_1Text;
+            text.text = string.Empty;
+            self.TextFomoPro.Add(text);
+
+            text = self.View.E_Label_ItemFumo_2Text;
+            text.text = string.Empty;
+            self.TextFomoPro.Add(text);
         }
 
         public static void OnInitUI(this DlgItemFumoSelect self, BagInfo fumoitem)
         {
-            // self.FumoItemInfo = fumoitem;
-            // ItemConfig itemConfig = ItemConfigCategory.Instance.Get(fumoitem.ItemID);
-            // string[] itemparams = itemConfig.ItemUsePar.Split('@');
-            // int weizhi = int.Parse(itemparams[0]);
-            // List<BagInfo> equipinfos = self.ZoneScene().GetComponent<BagComponent>().GetEquipListByWeizhi(weizhi);
-            //
-            // for (int i = 0; i < equipinfos.Count; i++)
-            // {
-            //     self.ItemList[i].GameObject.SetActive(true);
-            //     self.ItemList[i].UpdateItem(equipinfos[i], ItemOperateEnum.None);
-            //     self.ItemList[i].ShowTip = false;
-            //
-            //     self.TextFomoPro[i].text = ItemViewHelp.GetFumpProDesc(equipinfos[i].FumoProLists);
-            // }
-            //
-            // self.BagInfos = equipinfos;
+            self.FumoItemInfo = fumoitem;
+            ItemConfig itemConfig = ItemConfigCategory.Instance.Get(fumoitem.ItemID);
+            string[] itemparams = itemConfig.ItemUsePar.Split('@');
+            int weizhi = int.Parse(itemparams[0]);
+            List<BagInfo> equipinfos = self.Root().GetComponent<BagComponentC>().GetEquipListByWeizhi(weizhi);
+
+            for (int i = 0; i < equipinfos.Count; i++)
+            {
+                ES_CommonItem item = self.ItemList[i];
+                item.uiTransform.gameObject.SetActive(true);
+                item.UpdateItem(equipinfos[i], ItemOperateEnum.None);
+                item.ShowTip = false;
+
+                self.TextFomoPro[i].text = ItemViewHelp.GetFumpProDesc(equipinfos[i].FumoProLists);
+            }
+
+            self.BagInfos = equipinfos;
         }
 
         public static void OnCloseTips(this DlgItemFumoSelect self)
         {
-            // if (self.IsDisposed)
-            // {
-            //     return;
-            // }
-            //
-            // UIHelper.Remove(self.DomainScene(), UIType.UIItemFumoSelect);
+            if (self.IsDisposed)
+            {
+                return;
+            }
+
+            self.Root().GetComponent<UIComponent>().CloseWindow(WindowID.WindowID_ItemFumoSelect);
         }
 
         public static async ETTask OnSetClickHandler(this DlgItemFumoSelect self, BagInfo bagInfo)
         {
-            // int index = self.BagInfos.IndexOf(bagInfo);
-            // if (index == -1)
-            // {
-            //     index = 0;
-            // }
-            //
-            // BagInfo equipinfo = self.BagInfos[index];
-            // ItemConfig itemConfig = ItemConfigCategory.Instance.Get(self.FumoItemInfo.ItemID);
-            // List<HideProList> hideProLists = XiLianHelper.GetItemFumoPro(itemConfig.Id);
-            // string itemfumo = ItemViewHelp.GetFumpProDesc(hideProLists);
-            // if (equipinfo.FumoProLists.Count > 0)
-            // {
-            //     string equipfumo = ItemViewHelp.GetFumpProDesc(equipinfo.FumoProLists);
-            //     string fumopro = $"当前附魔属性<color=#BEFF34>{equipfumo}</color> \n是否覆盖已有属性\n{itemfumo}\n此附魔道具已消耗";
-            //     self.ZoneScene().GetComponent<BagComponent>().SendFumoUse(self.FumoItemInfo, hideProLists).Coroutine();
-            //     PopupTipHelp.OpenPopupTip(self.ZoneScene(), "装备附魔", fumopro, async () =>
-            //     {
-            //         await self.ZoneScene().GetComponent<BagComponent>().SendFumoPro(index);
-            //         FloatTipManager.Instance.ShowFloatTip($"附魔属性 {itemfumo}");
-            //
-            //         UIHelper.Remove(self.ZoneScene(), UIType.UIItemFumoSelect);
-            //     }, () => { UIHelper.Remove(self.ZoneScene(), UIType.UIItemFumoSelect); }).Coroutine();
-            // }
-            // else
-            // {
-            //     //await self.ZoneScene().GetComponent<BagComponent>().SendUseItem(self.FumoItemInfo, index.ToString());
-            //     await self.ZoneScene().GetComponent<BagComponent>().SendFumoUse(self.FumoItemInfo, hideProLists);
-            //     await self.ZoneScene().GetComponent<BagComponent>().SendFumoPro(index);
-            //     FloatTipManager.Instance.ShowFloatTip($"附魔属性 {itemfumo}");
-            //
-            //     UIHelper.Remove(self.ZoneScene(), UIType.UIItemFumoSelect);
-            // }
+            int index = self.BagInfos.IndexOf(bagInfo);
+            if (index == -1)
+            {
+                index = 0;
+            }
+
+            BagInfo equipinfo = self.BagInfos[index];
+            ItemConfig itemConfig = ItemConfigCategory.Instance.Get(self.FumoItemInfo.ItemID);
+            List<HideProList> hideProLists = XiLianHelper.GetItemFumoPro(itemConfig.Id);
+            string itemfumo = ItemViewHelp.GetFumpProDesc(hideProLists);
+            if (equipinfo.FumoProLists.Count > 0)
+            {
+                string equipfumo = ItemViewHelp.GetFumpProDesc(equipinfo.FumoProLists);
+                string fumopro = $"当前附魔属性<color=#BEFF34>{equipfumo}</color> \n是否覆盖已有属性\n{itemfumo}\n此附魔道具已消耗";
+                BagClientNetHelper.SendFumoUse(self.Root(), self.FumoItemInfo, hideProLists).Coroutine();
+                PopupTipHelp.OpenPopupTip(self.Root(), "装备附魔", fumopro, async () =>
+                {
+                    await BagClientNetHelper.SendFumoPro(self.Root(), index);
+                    FlyTipComponent.Instance.ShowFlyTip($"附魔属性 {itemfumo}");
+
+                    self.Root().GetComponent<UIComponent>().CloseWindow(WindowID.WindowID_ItemFumoSelect);
+                }, () => { self.Root().GetComponent<UIComponent>().CloseWindow(WindowID.WindowID_ItemFumoSelect); }).Coroutine();
+            }
+            else
+            {
+                await BagClientNetHelper.SendFumoUse(self.Root(), self.FumoItemInfo, hideProLists);
+                await BagClientNetHelper.SendFumoPro(self.Root(), index);
+                FlyTipComponent.Instance.ShowFlyTip($"附魔属性 {itemfumo}");
+
+                self.Root().GetComponent<UIComponent>().CloseWindow(WindowID.WindowID_ItemFumoSelect);
+            }
+
             await ETTask.CompletedTask;
         }
     }
--- Unity/Assets/Scripts/ModelView/Client/MengJing/UI/DlgItemFumoSelect/DlgItemFumoSelect.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UI/DlgItemFumoSelect/DlgItemFumoSelect.cs
@@ -1,12 +1,16 @@
-﻿namespace ET.Client
+﻿using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace ET.Client
 {
-	 [ComponentOf(typeof(UIBaseWindow))]
-	public  class DlgItemFumoSelect :Entity,IAwake,IUILogic
-	{
+    [ComponentOf(typeof(UIBaseWindow))]
+    public class DlgItemFumoSelect : Entity, IAwake, IUILogic
+    {
+        public DlgItemFumoSelectViewComponent View { get => this.GetComponent<DlgItemFumoSelectViewComponent>(); }
 
-		public DlgItemFumoSelectViewComponent View { get => this.GetComponent<DlgItemFumoSelectViewComponent>();} 
-
-		 
-
-	}
+        public BagInfo FumoItemInfo;
+        public List<Text> TextFomoPro = new();
+        public List<EntityRef<ES_CommonItem>> ItemList = new();
+        public List<BagInfo> BagInfos = new();
+    }
 }
