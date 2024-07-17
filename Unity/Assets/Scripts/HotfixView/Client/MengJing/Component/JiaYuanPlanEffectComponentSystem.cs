82a392fb97dea435b873407327bb7fa6fce7b23b
FortuneCat0v0 <1142094319@qq.com>
2024-07-17 13:57:55 +0800

    .

---
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/Component/JiaYuanPlanEffectComponentSystem.cs
@@ -0,0 +1,12 @@
+﻿namespace ET.Client
+{
+    [FriendOf(typeof(JiaYuanPlanEffectComponent))]
+    [EntitySystemOf(typeof(JiaYuanPlanEffectComponent))]
+    public static partial class JiaYuanPlanEffectComponentSystem
+    {
+        [EntitySystem]
+        private static void Awake(this JiaYuanPlanEffectComponent self)
+        {
+        }
+    }
+}
