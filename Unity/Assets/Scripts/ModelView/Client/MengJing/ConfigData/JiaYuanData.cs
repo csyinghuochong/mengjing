73129c075843a6820a0d370fc2b56eb8ce1f5c0c
FortuneCat0v0 <1142094319@qq.com>
2024-07-17 11:27:01 +0800

    .

--- Unity/Assets/Scripts/ModelView/Client/MengJing/ConfigData/JiaYuanData.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/ConfigData/JiaYuanData.cs
@@ -11,15 +11,15 @@ namespace ET.Client
         [StaticField]
         public static List<Vector3> PlanPositionList = new()
         {
-            new Vector3(14.35f, 0f, -5.98f),
-            new Vector3(17.27f, 0f, -5.98f),
-            new Vector3(20.32f, 0f, -5.98f),
-            new Vector3(4f - 0.5f, 0f, -37.58f - 0.5f),
+            new Vector3(22.73f, 0.16f, -4.37f),
+            new Vector3(22.73f, 0.16f, -4.37f - 3f),
+            new Vector3(22.73f, 0.16f, -4.37f - 6f),
+            new Vector3(22.73f, 0.16f, -4.37f - 9f),
 
-            new Vector3(1.25f - 0.5f, 0f, -31.24f - 0.5f),
-            new Vector3(1.25f - 0.5f, 0f, -33.32f - 0.5f),
-            new Vector3(1.25f - 0.5f, 0f, -35.39f - 0.5f),
-            new Vector3(1.25f - 0.5f, 0f, -37.58f - 0.5f),
+            new Vector3(19.78f, 0.16f, -4.37f),
+            new Vector3(19.78f, 0.16f, -4.37f - 3f),
+            new Vector3(19.78f, 0.16f, -4.37f - 6f),
+            new Vector3(19.78f, 0.16f, -4.37f - 9f),
 
             new Vector3(-2f, 0f, -31.24f - 0.5f),
             new Vector3(-2f, 0f, -33.32f - 0.5f),
