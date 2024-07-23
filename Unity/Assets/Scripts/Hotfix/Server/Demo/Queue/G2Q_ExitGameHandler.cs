2f6db0d3fcf5d01aa35e2e2cb2c13ddb47dac628
tangchunguang <394266315@qq.com>
2024-07-23 16:43:55 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/Demo/Queue/G2Q_ExitGameHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/Demo/Queue/G2Q_ExitGameHandler.cs
@@ -2,12 +2,11 @@ namespace ET.Server
 {
 
     [MessageHandler(SceneType.Queue)]
-    public class G2Q_ExitGameHandler : MessageHandler<Scene, R2Q_EnterQueue, Q2R_EnterQueue>
+    public class G2Q_ExitGameHandler : MessageHandler<Scene, G2Q_ExitGame, Q2G_ExitGame>
     {
-        protected override async ETTask Run(Scene scene, R2Q_EnterQueue request, Q2R_EnterQueue response)
+        protected override async ETTask Run(Scene scene, G2Q_ExitGame request, Q2G_ExitGame response)
         {
-            scene.Root().GetComponent<TokenComponent>().Add(request.Account, request.Token, false);
-            response.QueueNumber = scene.Root().GetComponent<TokenComponent>().GetTotalNumber() + 1;
+
             
             await ETTask.CompletedTask;
         }
