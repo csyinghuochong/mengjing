807e9fb2606596b7aee74bd4af27bc67d28e6b94
FortuneCat0v0 <1142094319@qq.com>
2024-07-31 18:22:27 +0800

    .

---
+++ Unity/Assets/Scripts/Loader/AnimData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Animancer;
+using UnityEngine;
+
+namespace ET
+{
+    [Serializable]
+    public class MotionTransition : ClipTransition
+    {
+        public string StateName;
+
+        [SerializeField, Tooltip("Should Root Motion be enabled when this animation plays?")]
+        private bool _ApplyRootMotion;
+
+        public override void Apply(AnimancerState state)
+        {
+            base.Apply(state);
+            state.Root.Component.Animator.applyRootMotion = _ApplyRootMotion;
+        }
+    }
+
+    public class AnimData : MonoBehaviour
+    {
+        public MotionTransition[] Animations;
+    }
+}
