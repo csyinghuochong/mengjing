2c9af124413d1618ba1ba5a8a3e9b122468881a4
FortuneCat0v0 <1142094319@qq.com>
2024-08-02 12:19:44 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_SettlementRwardItemViewSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_SettlementRwardItemViewSystem.cs
@@ -1,20 +1,98 @@
-﻿
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
+
 namespace ET.Client
 {
-	[EntitySystemOf(typeof(Scroll_Item_SettlementRwardItem))]
-	public static partial class Scroll_Item_SettlementRwardItemSystem 
-	{
-		[EntitySystem]
-		private static void Awake(this Scroll_Item_SettlementRwardItem self )
-		{
-		}
+    [FriendOf(typeof(ES_CommonItem))]
+    [FriendOf(typeof(Scroll_Item_SettlementRwardItem))]
+    [EntitySystemOf(typeof(Scroll_Item_SettlementRwardItem))]
+    public static partial class Scroll_Item_SettlementRwardItemSystem
+    {
+        [EntitySystem]
+        private static void Awake(this Scroll_Item_SettlementRwardItem self)
+        {
+        }
 
-		[EntitySystem]
-		private static void Destroy(this Scroll_Item_SettlementRwardItem self )
-		{
-			self.DestroyWidget();
-		}
-	}
+        [EntitySystem]
+        private static void Destroy(this Scroll_Item_SettlementRwardItem self)
+        {
+            self.DestroyWidget();
+        }
+
+        public static void OnInitUI(this Scroll_Item_SettlementRwardItem self)
+        {
+            if (self.RewardItem != null)
+            {
+                BagInfo bagInfo = BagInfo.Create();
+                bagInfo.ItemID = self.RewardItem.ItemID;
+                bagInfo.ItemNum = self.RewardItem.ItemNum;
+                self.ES_CommonItem.UpdateItem(bagInfo, ItemOperateEnum.None);
+            }
+        }
+
+        public static void OnUpdateData(this Scroll_Item_SettlementRwardItem self, RewardItem rewardItem)
+        {
+            self.E_Image_bgOpenImage.gameObject.SetActive(false);
+            self.E_ImageButtonButton.AddListener(self.OnClickItem);
+            self.ES_CommonItem.uiTransform.gameObject.SetActive(false);
+            self.OnInitUI();
+
+            self.RewardItem = rewardItem;
+            if (self.ES_CommonItem != null)
+            {
+                BagInfo bagInfo = BagInfo.Create();
+                bagInfo.ItemID = self.RewardItem.ItemID;
+                bagInfo.ItemNum = self.RewardItem.ItemNum;
+                self.ES_CommonItem.UpdateItem(bagInfo, ItemOperateEnum.None);
+            }
+        }
+
+        public static void SetClickHandler(this Scroll_Item_SettlementRwardItem self, Action<int> action, int index)
+        {
+            self.Index = index;
+            self.ClickHandler = action;
+        }
+
+        public static void ShowRewardItem(this Scroll_Item_SettlementRwardItem self, string name)
+        {
+            self.E_TextNameText.text = name;
+            if (self.ES_CommonItem.uiTransform.gameObject.activeSelf)
+            {
+                return;
+            }
+
+            self.ES_CommonItem.uiTransform.gameObject.SetActive(true);
+            self.E_Image_bgOpenImage.gameObject.SetActive(true);
+            self.E_Image_bgImage.gameObject.SetActive(false);
+            self.DisableClick();
+        }
+
+        public static bool IsCanClicked(this Scroll_Item_SettlementRwardItem self)
+        {
+            return self.E_ImageButtonButton.gameObject.activeSelf;
+        }
+
+        public static void DisableClick(this Scroll_Item_SettlementRwardItem self)
+        {
+            self.E_ImageButtonButton.gameObject.SetActive(false);
+        }
+
+        public static void OnClickItem(this Scroll_Item_SettlementRwardItem self)
+        {
+            Unit unit = UnitHelper.GetMyUnitFromClientScene(self.Root());
+            if (self.Index >= 3 && !unit.IsYueKaStates())
+            {
+                FlyTipComponent.Instance.ShowFlyTip("周卡用户才能开启！");
+                return;
+            }
+
+            if (self.ES_CommonItem.uiTransform.gameObject.activeSelf)
+            {
+                return;
+            }
+
+            self.ClickHandler(self.Index);
+        }
+    }
 }
--- Unity/Assets/Scripts/ModelView/Client/MengJing/UIItemBehaviour/Item_SettlementRwardItem.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIItemBehaviour/Item_SettlementRwardItem.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 namespace ET.Client
@@ -6,6 +7,10 @@ namespace ET.Client
 	[EnableMethod]
 	public  class Scroll_Item_SettlementRwardItem : Entity,IAwake,IDestroy,IUIScrollItem<Scroll_Item_SettlementRwardItem> 
 	{
+		public RewardItem RewardItem;
+		public Action<int> ClickHandler;
+		public int Index = -1;
+		
 		public long DataId {get;set;}
 		private bool isCacheNode = false;
 		public void SetCacheMode(bool isCache)
