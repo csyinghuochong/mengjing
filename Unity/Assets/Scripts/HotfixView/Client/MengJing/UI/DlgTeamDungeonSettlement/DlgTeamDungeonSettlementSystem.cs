57c66a5a1d1774432e310364eb33bcec021930bb
FortuneCat0v0 <1142094319@qq.com>
2024-08-02 13:08:55 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgTeamDungeonSettlement/DlgTeamDungeonSettlementSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgTeamDungeonSettlement/DlgTeamDungeonSettlementSystem.cs
@@ -6,20 +6,16 @@ using UnityEngine.UI;
 
 namespace ET.Client
 {
-	[FriendOf(typeof(DlgTeamDungeonSettlement))]
-	public static  class DlgTeamDungeonSettlementSystem
-	{
+    [FriendOf(typeof(DlgTeamDungeonSettlement))]
+    public static class DlgTeamDungeonSettlementSystem
+    {
+        public static void RegisterUIEvent(this DlgTeamDungeonSettlement self)
+        {
+        }
 
-		public static void RegisterUIEvent(this DlgTeamDungeonSettlement self)
-		{
-		 
-		}
-
-		public static void ShowWindow(this DlgTeamDungeonSettlement self, Entity contextData = null)
-		{
-		}
-
-		 
-
-	}
+        public static void ShowWindow(this DlgTeamDungeonSettlement self, Entity contextData = null)
+        {
+        }
+        
+    }
 }
--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_TeamDungeonSettlementPlayerViewSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_TeamDungeonSettlementPlayerViewSystem.cs
@@ -1,20 +1,42 @@
-﻿
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 namespace ET.Client
 {
-	[EntitySystemOf(typeof(Scroll_Item_TeamDungeonSettlementPlayer))]
-	public static partial class Scroll_Item_TeamDungeonSettlementPlayerSystem 
-	{
-		[EntitySystem]
-		private static void Awake(this Scroll_Item_TeamDungeonSettlementPlayer self )
-		{
-		}
+    [FriendOf(typeof(ES_RewardList))]
+    [EntitySystemOf(typeof(Scroll_Item_TeamDungeonSettlementPlayer))]
+    public static partial class Scroll_Item_TeamDungeonSettlementPlayerSystem
+    {
+        [EntitySystem]
+        private static void Awake(this Scroll_Item_TeamDungeonSettlementPlayer self)
+        {
+        }
 
-		[EntitySystem]
-		private static void Destroy(this Scroll_Item_TeamDungeonSettlementPlayer self )
-		{
-			self.DestroyWidget();
-		}
-	}
+        [EntitySystem]
+        private static void Destroy(this Scroll_Item_TeamDungeonSettlementPlayer self)
+        {
+            self.DestroyWidget();
+        }
+
+        public static void OnUpdateUI(this Scroll_Item_TeamDungeonSettlementPlayer self, TeamPlayerInfo teamPlayerInfo, List<RewardItem> rewardItems)
+        {
+            self.ES_RewardList.uiTransform.gameObject.SetActive(false);
+
+            self.E_Text_NameText.text = teamPlayerInfo.PlayerName;
+            using (zstring.Block())
+            {
+                self.E_Text_LevelText.text = zstring.Format("等级：{0}", teamPlayerInfo.PlayerLv);
+                self.E_Text_DamageText.text = zstring.Format("伤害：{0}", teamPlayerInfo.Damage);
+                CommonViewHelper.ShowOccIcon(self.Root(), self.E_ImageIconImage.gameObject, teamPlayerInfo.Occ);
+
+                self.ES_RewardList.uiTransform.gameObject.SetActive(rewardItems != null);
+                if (rewardItems != null)
+                {
+                    string itemList = zstring.Format("{0};{1}", rewardItems[0].ItemID, rewardItems[0].ItemNum);
+                    self.ES_RewardList.Refresh(itemList, 0.6f);
+                }
+            }
+        }
+    }
 }
