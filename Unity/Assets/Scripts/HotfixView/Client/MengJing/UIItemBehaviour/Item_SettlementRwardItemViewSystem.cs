f4e5036eb0db8e73d52e44f4a288d5867b07d9d6
FortuneCat0v0 <1142094319@qq.com>
2024-08-02 12:07:51 +0800

    .

---
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_SettlementRwardItemViewSystem.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET.Client
+{
+	[EntitySystemOf(typeof(Scroll_Item_SettlementRwardItem))]
+	public static partial class Scroll_Item_SettlementRwardItemSystem 
+	{
+		[EntitySystem]
+		private static void Awake(this Scroll_Item_SettlementRwardItem self )
+		{
+		}
+
+		[EntitySystem]
+		private static void Destroy(this Scroll_Item_SettlementRwardItem self )
+		{
+			self.DestroyWidget();
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIItemBehaviour/Item_SettlementRwardItem.cs
@@ -0,0 +1,206 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET.Client
+{
+	[EnableMethod]
+	public  class Scroll_Item_SettlementRwardItem : Entity,IAwake,IDestroy,IUIScrollItem<Scroll_Item_SettlementRwardItem> 
+	{
+		public long DataId {get;set;}
+		private bool isCacheNode = false;
+		public void SetCacheMode(bool isCache)
+		{
+			this.isCacheNode = isCache;
+		}
+
+		public Scroll_Item_SettlementRwardItem BindTrans(Transform trans)
+		{
+			this.uiTransform = trans;
+			return this;
+		}
+
+		public UnityEngine.UI.Image E_Image_bgImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_Image_bgImage == null )
+     				{
+		    			this.m_E_Image_bgImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_Image_bg");
+     				}
+     				return this.m_E_Image_bgImage;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_Image_bg");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_Image_bgOpenImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_Image_bgOpenImage == null )
+     				{
+		    			this.m_E_Image_bgOpenImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_Image_bgOpen");
+     				}
+     				return this.m_E_Image_bgOpenImage;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_Image_bgOpen");
+     			}
+     		}
+     	}
+
+		public ES_CommonItem ES_CommonItem
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+		    	ES_CommonItem es = this.m_es_commonitem;
+     			if (this.isCacheNode)
+     			{
+     				if( es == null )
+
+     				{
+		    			Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"ES_CommonItem");
+		    			this.m_es_commonitem = this.AddChild<ES_CommonItem,Transform>(subTrans);
+     				}
+     				return this.m_es_commonitem;
+     			}
+     			else
+     			{
+     				if( es != null )
+
+     				{
+		    			Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"ES_CommonItem");
+		    			es = this.m_es_commonitem;
+     					if( es.UITransform != subTrans )
+     					{
+     						es.Dispose();
+		    				this.m_es_commonitem = null;
+		    				this.m_es_commonitem = this.AddChild<ES_CommonItem,Transform>(subTrans);
+     					}
+     				}
+     				else
+     				{
+		    			Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"ES_CommonItem");
+		    			this.m_es_commonitem = this.AddChild<ES_CommonItem,Transform>(subTrans);
+     				}
+     				return this.m_es_commonitem;
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_ImageButtonButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_ImageButtonButton == null )
+     				{
+		    			this.m_E_ImageButtonButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_ImageButton");
+     				}
+     				return this.m_E_ImageButtonButton;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"E_ImageButton");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_ImageButtonImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_ImageButtonImage == null )
+     				{
+		    			this.m_E_ImageButtonImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_ImageButton");
+     				}
+     				return this.m_E_ImageButtonImage;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_ImageButton");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_TextNameText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_TextNameText == null )
+     				{
+		    			this.m_E_TextNameText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_TextName");
+     				}
+     				return this.m_E_TextNameText;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_TextName");
+     			}
+     		}
+     	}
+
+		public void DestroyWidget()
+		{
+			this.m_E_Image_bgImage = null;
+			this.m_E_Image_bgOpenImage = null;
+			this.m_es_commonitem = null;
+			this.m_E_ImageButtonButton = null;
+			this.m_E_ImageButtonImage = null;
+			this.m_E_TextNameText = null;
+			this.uiTransform = null;
+			this.DataId = 0;
+		}
+
+		private UnityEngine.UI.Image m_E_Image_bgImage = null;
+		private UnityEngine.UI.Image m_E_Image_bgOpenImage = null;
+		private EntityRef<ES_CommonItem> m_es_commonitem = null;
+		private UnityEngine.UI.Button m_E_ImageButtonButton = null;
+		private UnityEngine.UI.Image m_E_ImageButtonImage = null;
+		private UnityEngine.UI.Text m_E_TextNameText = null;
+		public Transform uiTransform = null;
+	}
+}
