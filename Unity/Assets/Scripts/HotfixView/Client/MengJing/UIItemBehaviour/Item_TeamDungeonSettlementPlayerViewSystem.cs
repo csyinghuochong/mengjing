afe63577d1bd86aa41b12fb0598ab788f1c6564d
FortuneCat0v0 <1142094319@qq.com>
2024-08-02 12:31:43 +0800

    .

---
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_TeamDungeonSettlementPlayerViewSystem.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET.Client
+{
+	[EntitySystemOf(typeof(Scroll_Item_TeamDungeonSettlementPlayer))]
+	public static partial class Scroll_Item_TeamDungeonSettlementPlayerSystem 
+	{
+		[EntitySystem]
+		private static void Awake(this Scroll_Item_TeamDungeonSettlementPlayer self )
+		{
+		}
+
+		[EntitySystem]
+		private static void Destroy(this Scroll_Item_TeamDungeonSettlementPlayer self )
+		{
+			self.DestroyWidget();
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIItemBehaviour/Item_TeamDungeonSettlementPlayer.cs
@@ -0,0 +1,180 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET.Client
+{
+	[EnableMethod]
+	public  class Scroll_Item_TeamDungeonSettlementPlayer : Entity,IAwake,IDestroy,IUIScrollItem<Scroll_Item_TeamDungeonSettlementPlayer> 
+	{
+		public long DataId {get;set;}
+		private bool isCacheNode = false;
+		public void SetCacheMode(bool isCache)
+		{
+			this.isCacheNode = isCache;
+		}
+
+		public Scroll_Item_TeamDungeonSettlementPlayer BindTrans(Transform trans)
+		{
+			this.uiTransform = trans;
+			return this;
+		}
+
+		public UnityEngine.UI.Image E_ImageIconImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_ImageIconImage == null )
+     				{
+		    			this.m_E_ImageIconImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_ImageIcon");
+     				}
+     				return this.m_E_ImageIconImage;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"E_ImageIcon");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_Text_NameText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_Text_NameText == null )
+     				{
+		    			this.m_E_Text_NameText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_Text_Name");
+     				}
+     				return this.m_E_Text_NameText;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_Text_Name");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_Text_LevelText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_Text_LevelText == null )
+     				{
+		    			this.m_E_Text_LevelText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_Text_Level");
+     				}
+     				return this.m_E_Text_LevelText;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_Text_Level");
+     			}
+     		}
+     	}
+
+		public UnityEngine.UI.Text E_Text_DamageText
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if (this.isCacheNode)
+     			{
+     				if( this.m_E_Text_DamageText == null )
+     				{
+		    			this.m_E_Text_DamageText = UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_Text_Damage");
+     				}
+     				return this.m_E_Text_DamageText;
+     			}
+     			else
+     			{
+		    		return UIFindHelper.FindDeepChild<UnityEngine.UI.Text>(this.uiTransform.gameObject,"E_Text_Damage");
+     			}
+     		}
+     	}
+
+		public ES_RewardList ES_RewardList
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+		    	ES_RewardList es = this.m_es_rewardlist;
+     			if (this.isCacheNode)
+     			{
+     				if( es == null )
+
+     				{
+		    			Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"ES_RewardList");
+		    			this.m_es_rewardlist = this.AddChild<ES_RewardList,Transform>(subTrans);
+     				}
+     				return this.m_es_rewardlist;
+     			}
+     			else
+     			{
+     				if( es != null )
+
+     				{
+		    			Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"ES_RewardList");
+		    			es = this.m_es_rewardlist;
+     					if( es.UITransform != subTrans )
+     					{
+     						es.Dispose();
+		    				this.m_es_rewardlist = null;
+		    				this.m_es_rewardlist = this.AddChild<ES_RewardList,Transform>(subTrans);
+     					}
+     				}
+     				else
+     				{
+		    			Transform subTrans = UIFindHelper.FindDeepChild<Transform>(this.uiTransform.gameObject,"ES_RewardList");
+		    			this.m_es_rewardlist = this.AddChild<ES_RewardList,Transform>(subTrans);
+     				}
+     				return this.m_es_rewardlist;
+     			}
+     		}
+     	}
+
+		public void DestroyWidget()
+		{
+			this.m_E_ImageIconImage = null;
+			this.m_E_Text_NameText = null;
+			this.m_E_Text_LevelText = null;
+			this.m_E_Text_DamageText = null;
+			this.m_es_rewardlist = null;
+			this.uiTransform = null;
+			this.DataId = 0;
+		}
+
+		private UnityEngine.UI.Image m_E_ImageIconImage = null;
+		private UnityEngine.UI.Text m_E_Text_NameText = null;
+		private UnityEngine.UI.Text m_E_Text_LevelText = null;
+		private UnityEngine.UI.Text m_E_Text_DamageText = null;
+		private EntityRef<ES_RewardList> m_es_rewardlist = null;
+		public Transform uiTransform = null;
+	}
+}
