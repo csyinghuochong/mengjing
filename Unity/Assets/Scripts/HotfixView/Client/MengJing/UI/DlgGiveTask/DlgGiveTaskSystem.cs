1025075d50aa41459f8a7b39956e38340c4ff8f7
FortuneCat0v0 <1142094319@qq.com>
2024-08-07 10:59:19 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgGiveTask/DlgGiveTaskSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgGiveTask/DlgGiveTaskSystem.cs
@@ -6,20 +6,91 @@ using UnityEngine.UI;
 
 namespace ET.Client
 {
-	[FriendOf(typeof(DlgGiveTask))]
-	public static  class DlgGiveTaskSystem
-	{
+    [FriendOf(typeof(Scroll_Item_CommonItem))]
+    [FriendOf(typeof(DlgGiveTask))]
+    public static class DlgGiveTaskSystem
+    {
+        public static void RegisterUIEvent(this DlgGiveTask self)
+        {
+            self.View.E_BagItemsLoopVerticalScrollRect.AddItemRefreshListener(self.OnBagItemsRefresh);
+            self.View.E_CloseBtnButton.AddListener(self.OnCloseBtn);
+            self.View.E_GiveBtnButton.AddListenerAsync(self.OnGiveBtn);
+        }
 
-		public static void RegisterUIEvent(this DlgGiveTask self)
-		{
-		 
-		}
+        public static void ShowWindow(this DlgGiveTask self, Entity contextData = null)
+        {
+            self.OnBagListUpdate();
+        }
 
-		public static void ShowWindow(this DlgGiveTask self, Entity contextData = null)
-		{
-		}
+        public static void InitTask(this DlgGiveTask self, int taskId, int taskType = 1)
+        {
+            self.TaskId = taskId;
 
-		 
+            TaskConfig taskConfig = TaskConfigCategory.Instance.Get(taskId);
+            self.View.E_TaskDesTextText.text = taskConfig.TaskDes;
+        }
 
-	}
+        public static void OnCloseBtn(this DlgGiveTask self)
+        {
+            self.Root().GetComponent<UIComponent>().CloseWindow(WindowID.WindowID_GiveTask);
+        }
+
+        private static void OnBagItemsRefresh(this DlgGiveTask self, Transform transform, int index)
+        {
+            Scroll_Item_CommonItem scrollItemCommonItem = self.ScrollItemCommonItems[index].BindTrans(transform);
+            scrollItemCommonItem.Refresh(self.ShowBagInfos[index], ItemOperateEnum.SkillSet, self.OnSelect);
+        }
+
+        public static void OnBagListUpdate(this DlgGiveTask self)
+        {
+            BagComponentC bagComponent = self.Root().GetComponent<BagComponentC>();
+
+            self.ShowBagInfos.Clear();
+            self.ShowBagInfos.AddRange(bagComponent.GetItemsByType(ItemTypeEnum.Equipment));
+
+            self.AddUIScrollItems(ref self.ScrollItemCommonItems, self.ShowBagInfos.Count);
+            self.View.E_BagItemsLoopVerticalScrollRect.SetVisible(true, self.ShowBagInfos.Count);
+        }
+
+        public static void OnSelect(this DlgGiveTask self, BagInfo bagInfo)
+        {
+            self.BagInfo = bagInfo;
+            self.View.ES_CommonItem.UpdateItem(bagInfo, ItemOperateEnum.None);
+            if (self.ScrollItemCommonItems != null)
+            {
+                foreach (Scroll_Item_CommonItem item in self.ScrollItemCommonItems.Values)
+                {
+                    if (item.uiTransform == null)
+                    {
+                        continue;
+                    }
+
+                    item.ES_CommonItem.SetSelected(bagInfo);
+                }
+            }
+        }
+
+        public static async ETTask OnGiveBtn(this DlgGiveTask self)
+        {
+            TaskComponentC taskComponent = self.Root().GetComponent<TaskComponentC>();
+
+            TaskConfig taskConfig = TaskConfigCategory.Instance.Get(self.TaskId);
+            if (TaskHelper.IsTaskGiveItem(taskConfig.TargetType, taskConfig.Target, taskConfig.TargetValue, self.BagInfo))
+            {
+                TaskPro taskPro = taskComponent.GetTaskById(self.TaskId);
+                taskPro.taskStatus = (int)TaskStatuEnum.Completed;
+                int errorCode = await TaskClientNetHelper.RequestCommitTask(self.Root(), self.TaskId, self.BagInfo.BagInfoID);
+                if (errorCode == ErrorCode.ERR_Success)
+                {
+                    FlyTipComponent.Instance.ShowFlyTip("完成任务");
+                    self.OnGiveAction?.Invoke();
+                    self.Root().GetComponent<UIComponent>().CloseWindow(WindowID.WindowID_GiveTask);
+                }
+            }
+            else
+            {
+                FlyTipComponent.Instance.ShowFlyTip("道具类型不符合任务要求");
+            }
+        }
+    }
 }
--- Unity/Assets/Scripts/ModelView/Client/MengJing/UI/DlgGiveTask/DlgGiveTask.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UI/DlgGiveTask/DlgGiveTask.cs
@@ -1,12 +1,18 @@
-﻿namespace ET.Client
+﻿using System;
+using System.Collections.Generic;
+
+namespace ET.Client
 {
-	 [ComponentOf(typeof(UIBaseWindow))]
-	public  class DlgGiveTask :Entity,IAwake,IUILogic
-	{
+    [ComponentOf(typeof(UIBaseWindow))]
+    public class DlgGiveTask : Entity, IAwake, IUILogic
+    {
+        public DlgGiveTaskViewComponent View { get => this.GetComponent<DlgGiveTaskViewComponent>(); }
 
-		public DlgGiveTaskViewComponent View { get => this.GetComponent<DlgGiveTaskViewComponent>();} 
+        public int TaskId;
 
-		 
-
-	}
+        public BagInfo BagInfo;
+        public Dictionary<int, EntityRef<Scroll_Item_CommonItem>> ScrollItemCommonItems;
+        public List<BagInfo> ShowBagInfos = new();
+        public Action OnGiveAction;
+    }
 }
