e2fc7a7cff6ee61c8e62d66e1767e4de7742d4c9
tangchunguang <394266315@qq.com>
2024-08-08 12:01:13 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Client/MengJing/Chat/Handler/M2C_SyncChatInfoHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Client/MengJing/Chat/Handler/M2C_SyncChatInfoHandler.cs
@@ -1,9 +1,9 @@
 ﻿namespace ET.Client
 {
     [MessageHandler(SceneType.Demo)]
-    public class C2C_SyncChatInfoHandler : MessageHandler<Scene, C2C_SyncChatInfo>
+    public class M2C_SyncChatInfoHandler : MessageHandler<Scene, M2C_SyncChatInfo>
     {
-        protected override async ETTask Run(Scene root, C2C_SyncChatInfo message)
+        protected override async ETTask Run(Scene root, M2C_SyncChatInfo message)
         {
             HintHelp.ShowHint(root, $"收到 频道：{message.ChatInfo.ChannelId} 玩家：{message.ChatInfo.PlayerName} 消息：{message.ChatInfo.ChatMsg}");
 
