a14d622803155819b79f8e2dee6eb7c52542340c
FortuneCat0v0 <1142094319@qq.com>
2024-08-08 12:06:44 +0800

    .

--- Unity/Assets/Scripts/Hotfix/Client/MengJing/Main/Handler/M2C_HappyInfoHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Client/MengJing/Main/Handler/M2C_HappyInfoHandler.cs
@@ -1,11 +1,10 @@
 namespace ET.Client
 {
     [MessageHandler(SceneType.Demo)]
-    public class M2C_HappyInfoHandler: MessageHandler<Scene, M2C_HappyInfoResult>
+    public class M2C_HappyInfoHandler : MessageHandler<Scene, M2C_HappyInfoResult>
     {
         protected override async ETTask Run(Scene root, M2C_HappyInfoResult message)
         {
-            HintHelp.ShowHint(root, "to do : M2C_HappyInfoResult");
             root.GetComponent<BattleMessageComponent>().M2C_HappyInfoResult = message;
 
             EventSystem.Instance.Publish(root, new HappyInfo() { M2CHappyInfoResult = message });
