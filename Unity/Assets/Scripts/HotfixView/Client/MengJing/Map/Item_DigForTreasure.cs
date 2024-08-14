0e4903f76baf5b8014776a5f366cfefbc25a7ab0
FortuneCat0v0 <1142094319@qq.com>
2024-08-14 19:11:31 +0800

    .

---
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/Map/Item_DigForTreasure.cs
@@ -0,0 +1,21 @@
+﻿namespace ET.Client
+{
+    [Event(SceneType.Demo)]
+    public class Item_DigForTreasure : AEvent<Scene, DigForTreasure>
+    {
+        protected override async ETTask Run(Scene scene, DigForTreasure args)
+        {
+            ItemConfig itemConfig = ItemConfigCategory.Instance.Get(args.BagInfo.ItemID);
+            if (itemConfig.ItemSubType == 113)
+            {
+                // scene.GetComponent<UIComponent>().GetDlgLogic<DlgMain>().View.UIDigTreasureComponent.OnInitUI(args.BagInfo);
+            }
+
+            if (itemConfig.ItemSubType == 127)
+            {
+                await scene.GetComponent<UIComponent>().ShowWindowAsync(WindowID.WindowID_TreasureOpen);
+                scene.GetComponent<UIComponent>().GetDlgLogic<DlgTreasureOpen>().OnInitUI(args.BagInfo);
+            }
+        }
+    }
+}
