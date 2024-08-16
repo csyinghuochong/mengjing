767e68325e1eea9e474c71b3991cc0f52c328f77
tangchunguang <394266315@qq.com>
2024-08-16 17:37:44 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/Demo/Robot/Event/Team_OnRecvTeamDungeonOpen.cs
+++ Unity/Assets/Scripts/Hotfix/Server/Demo/Robot/Event/Team_OnRecvTeamDungeonOpen.cs
@@ -6,9 +6,8 @@ public class Team_OnRecvTeamDungeonOpen : AEvent<Scene, RecvTeamDungeonOpen>
 {
     protected override async ETTask Run(Scene scene, RecvTeamDungeonOpen args)
     {
-        
         Console.WriteLine("Team_OnRecvTeamDungeonOpen");
-
+        await scene.Root().GetComponent<TimerComponent>().WaitAsync(2000);
         TeamNetHelper.TeamDungeonPrepareRequest( scene.Root(), args.TeamInfo, 1 ).Coroutine();
         await ETTask.CompletedTask;
     }
