4311c3ffbf97c762f8c2c550cbec8e2a7ac098e4
FortuneCat0v0 <1142094319@qq.com>
2024-08-20 14:29:42 +0800

    .

--- Unity/Assets/Scripts/Hotfix/Client/MengJing/Main/Handler/M2C_JiaYuanUpdateHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Client/MengJing/Main/Handler/M2C_JiaYuanUpdateHandler.cs
@@ -1,11 +1,13 @@
 namespace ET.Client
 {
     [MessageHandler(SceneType.Demo)]
-    public class M2C_JiaYuanUpdateHandler: MessageHandler<Scene, M2C_JiaYuanUpdate>
+    public class M2C_JiaYuanUpdateHandler : MessageHandler<Scene, M2C_JiaYuanUpdate>
     {
         protected override async ETTask Run(Scene root, M2C_JiaYuanUpdate message)
         {
-          
+            JiaYuanComponentC jiaYuanComponent = root.GetComponent<JiaYuanComponentC>();
+            jiaYuanComponent.PurchaseItemList_7 = message.PurchaseItemList;
+
             await ETTask.CompletedTask;
         }
     }
