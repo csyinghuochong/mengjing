dd7b3db5bf57ca60895aee2f8ed094ece9e00be1
FortuneCat0v0 <1142094319@qq.com>
2024-08-20 10:48:05 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/Demo/Opera/OperaComponentSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/Opera/OperaComponentSystem.cs
@@ -127,7 +127,7 @@ namespace ET.Client
                 //     return;
                 // }
                 // 测试技能
-                List<int> skillids = new List<int>() { 62001206 };
+                List<int> skillids = new List<int>() { 61022211 };
                 Unit unit = UnitHelper.GetMyUnitFromClientScene(self.Root());
                 long targetId = self.Root().GetComponent<LockTargetComponent>().LastLockId;
 
