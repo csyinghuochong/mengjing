058a12db9fec2e4199fbc7e26b7130d53a895169
FortuneCat0v0 <1142094319@qq.com>
2024-08-21 14:32:42 +0800

    .

---
+++ Unity/Assets/Scripts/Hotfix/Client/MengJing/Main/Handler/M2C_SoloDungeonHandle.cs
@@ -0,0 +1,14 @@
+﻿namespace ET.Client
+{
+    public class M2C_SoloDungeonHandle : MessageHandler<Scene, M2C_SoloDungeon>
+    {
+        protected override async ETTask Run(Scene root, M2C_SoloDungeon message)
+        {
+            Log.Debug("恭喜你！竞技场获胜...." + message.SoloResult + "并且获得奖励:" + message.RewardItem[0].ItemID + ";" + message.RewardItem[0].ItemNum);
+
+            EventSystem.Instance.Publish(root, new UISoloReward() { m2C_SoloDungeon = message });
+
+            await ETTask.CompletedTask;
+        }
+    }
+}
