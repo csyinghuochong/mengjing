2bb9749f981a7e8768e62db7a0288a0a48db1d4e
FortuneCat0v0 <1142094319@qq.com>
2024-08-22 19:22:55 +0800

    .

---
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/Skill/Event/Skill_SkillEffectMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace ET.Client
+{
+    [Event(SceneType.Demo)]
+    public class Skill_SkillEffectMove : AEvent<Scene, SkillEffectMove>
+    {
+        protected override async ETTask Run(Scene scene, SkillEffectMove args)
+        {
+            EffectViewComponent effectViewComponent = args.Unit.GetComponent<EffectViewComponent>();
+            if (effectViewComponent == null)
+            {
+                return;
+            }
+
+            Effect effect = effectViewComponent.GetEffect(args.EffectInstanceId);
+            if (effect != null)
+            {
+                effect.UpdateEffectPosition(args.Postion, args.Angle);
+            }
+
+            await ETTask.CompletedTask;
+        }
+    }
+}
