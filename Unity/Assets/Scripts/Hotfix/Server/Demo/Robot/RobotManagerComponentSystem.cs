e9b1bb7e92af3ad4f39a71314e3e37c9c52fe3be
tangchunguang2 <2322657661@qq.com>
2024-08-27 19:05:53 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/Demo/Robot/RobotManagerComponentSystem.cs
+++ Unity/Assets/Scripts/Hotfix/Server/Demo/Robot/RobotManagerComponentSystem.cs
@@ -25,32 +25,49 @@ namespace ET.Server
             }
         }
 
-        public static int GetRobotNumber(this RobotManagerComponent self, int zone, int robotid)
+        public static string GetRobotNumber(this RobotManagerComponent self, int zone, string robotid)
         {
-            int number = 0;
+            int robotNumber = 0;
+            string account = string.Empty;
             foreach (var infos in self.robots.Values)
             {
-                if (infos.Key == zone && infos.Value == robotid)
+                if (infos.KeyId  == -1)
                 {
-                    number++;
+                    //该账号已经回收 可以直接拿来使用
+                    account = infos.Value2;
+                    break;
+                }
+
+                if (infos.KeyId == zone && infos.Value == robotid)
+                {
+                    robotNumber++;
                 }
             }
-
-            return number;
+            account = $"{zone}_{robotid}_{robotNumber}_0001";   //服务器
+            return account;
         }
 
         public static async ETTask RemoveRobot(this RobotManagerComponent self, int fiberid)
         {
+            KeyValuePair infos = null;
+            self.robots.TryGetValue(fiberid, out infos);
             
+            if (infos != null && infos.KeyId != -1 )
+            {
+                infos.KeyId = -1;
+                FiberManager.Instance.Remove(fiberid).Coroutine();
+            }
+
             await ETTask.CompletedTask;
         }
 
         public static async ETTask<int> NewRobot(this RobotManagerComponent self, int zone, int robotid)
         {
-            int robotNumber = self.GetRobotNumber(zone, robotid);
-            string account = $"{zone}_{robotid}_{robotNumber}_0001";   //服务器
+            string account = self.GetRobotNumber(zone, robotid.ToString());
+            
             int fiberId = await FiberManager.Instance.Create(SchedulerType.ThreadPool, self.Zone(), SceneType.Robot, account);
-            KeyValuePair<int, int> infos = new KeyValuePair<int, int>(zone, robotid);
+            KeyValuePair infos = new KeyValuePair() { KeyId = zone, Value = robotid.ToString(), Value2 = account };
+            
             self.robots.Add(fiberId, infos);
             return fiberId;
         }
--- Unity/Assets/Scripts/Model/Server/Demo/Robot/RobotManagerComponent.cs
+++ Unity/Assets/Scripts/Model/Server/Demo/Robot/RobotManagerComponent.cs
@@ -5,7 +5,7 @@ namespace ET.Server
     [ComponentOf(typeof(Scene))]
     public class RobotManagerComponent: Entity, IAwake, IDestroy
     {
-        public Dictionary<int,KeyValuePair<int,int>> robots = new();
+        public Dictionary<int,KeyValuePair> robots = new();
 
         public Dictionary<long, long> TeamRobot { get; set; } = new Dictionary<long, long>();
     }
