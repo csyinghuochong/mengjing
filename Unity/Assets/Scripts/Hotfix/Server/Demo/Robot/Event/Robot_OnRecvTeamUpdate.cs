c6db2681f93fabe050b2d2723f369eb67b90d05c
tangchunguang2 <2322657661@qq.com>
2024-08-27 19:43:53 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/Demo/Robot/Event/Robot_OnRecvTeamUpdate.cs
+++ Unity/Assets/Scripts/Hotfix/Server/Demo/Robot/Event/Robot_OnRecvTeamUpdate.cs
@@ -9,14 +9,21 @@ public class Robot_OnRecvTeamUpdate : AEvent<Scene, RecvTeamUpdate>
     protected override async ETTask Run(Scene scene, RecvTeamUpdate args)
     {
         Console.WriteLine("Robot_OnRecvTeamUpdate");
-        
-        ActorId roborActorId = UnitCacheHelper.GetRobotServerId();
-        Main2RobotManager_Message main2RobotClientMessage = Main2RobotManager_Message.Create();
-        main2RobotClientMessage.OpType = 1;
-        main2RobotClientMessage.OpParam = scene.Fiber.Id.ToString();
-        RobotManager2Main_Message respone =
-                await scene.Root().GetComponent<ProcessInnerSender>().Call(roborActorId, main2RobotClientMessage) as
-                        RobotManager2Main_Message;
+
+        TeamInfo selfinfo = scene.GetComponent<TeamComponentC>().GetSelfTeam();
+
+        if (selfinfo == null)
+        {
+            ActorId roborActorId = UnitCacheHelper.GetRobotServerId();
+            Main2RobotManager_Message main2RobotClientMessage = Main2RobotManager_Message.Create();
+            main2RobotClientMessage.OpType = 1;
+            main2RobotClientMessage.OpParam = scene.Fiber.Id.ToString();
+            RobotManager2Main_Message respone =
+                    await scene.Root().GetComponent<ProcessInnerSender>().Call(roborActorId, main2RobotClientMessage) as
+                            RobotManager2Main_Message;
+        }
+
+      
         
         await ETTask.CompletedTask;
     }
--- Unity/Assets/Scripts/Hotfix/Server/Demo/Robot/Handler/Main2RobotManager_MessageHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/Demo/Robot/Handler/Main2RobotManager_MessageHandler.cs
@@ -5,16 +5,16 @@ using ET.Server;
 
 namespace ET.Client
 {
-    [MessageHandler(SceneType.Demo)]
+    [MessageHandler(SceneType.RobotManager)]
     public class Main2RobotManager_MessageHandler : MessageHandler<Scene, Main2RobotManager_Message, RobotManager2Main_Message>
     {
         protected override async ETTask Run(Scene root, Main2RobotManager_Message request, RobotManager2Main_Message response)
         {
-            
-            Console.WriteLine("Main2RobotManager_Message");
             switch (request.OpType)
             {
                 case 1:
+                    
+                    Console.WriteLine("Main2RobotManager_Message.RemoveRobot");
                     root.GetComponent<RobotManagerComponent>().RemoveRobot(int.Parse(request.OpParam)).Coroutine();
                     break;
                 default:
