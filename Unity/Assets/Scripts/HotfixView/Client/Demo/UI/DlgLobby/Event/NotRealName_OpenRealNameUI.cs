7d32d5f2214caeddfb34c1a43d6762712a2eee15
tangchunguang <394266315@qq.com>
2024-09-05 11:20:00 +0800

    no message

--- Unity/Assets/Scripts/HotfixView/Client/Demo/UI/DlgLobby/Event/NotRealName_OpenRealNameUI.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/UI/DlgLobby/Event/NotRealName_OpenRealNameUI.cs
@@ -6,20 +6,7 @@ namespace ET.Client
         protected override async ETTask Run(Scene scene, NotRealName args)
         {
             //弹出实名认证窗口。
-            string name = "余传建";
-            string idcard = "41152419930913205X";
-
-            await scene.GetComponent<TimerComponent>().WaitAsync(2000);
-            int errorCode = await LoginHelper.RealName(scene, name, idcard);
-            
-            //实名认证成功再登陆。
-            if (errorCode != ErrorCode.ERR_Success)
-            {
-                return;
-            }
-
-            PlayerComponent playerComponent = scene.GetComponent<PlayerComponent>();
-            await LoginHelper.Login(scene.Root(), playerComponent.Account, playerComponent.Password, 0,playerComponent.VersionMode);
+            await scene.Root().GetComponent<UIComponent>().ShowWindowAsync(WindowID.WindowID_RealName);
             
             await ETTask.CompletedTask;
         }
--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgRealName/DlgRealNameSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgRealName/DlgRealNameSystem.cs
@@ -12,14 +12,31 @@ namespace ET.Client
 
 		public static void RegisterUIEvent(this DlgRealName self)
 		{
-		 
+			self.View.E_RealName_BtnButton.AddListenerAsync(self.OnRealName_Btn);
 		}
 
 		public static void ShowWindow(this DlgRealName self, Entity contextData = null)
 		{
 		}
 
-		 
+		public static async ETTask OnRealName_Btn(this DlgRealName self)
+		{
+			string name = "余传建";
+			string idcard = "41152419930913205X";
+			
+			int errorCode = await LoginHelper.RealName(self.Root(), name, idcard);
+            
+			//实名认证成功再登陆。
+			if (errorCode != ErrorCode.ERR_Success)
+			{
+				return;
+			}
+			
+			DlgMJLogin dlg =  self.Root().GetComponent<UIComponent>().GetDlgLogic<DlgMJLogin>();
+			dlg.OnLoginButton().Coroutine();
+
+			self.Root().GetComponent<UIComponent>().CloseWindow(WindowID.WindowID_RealName);
+		}
 
 	}
 }
