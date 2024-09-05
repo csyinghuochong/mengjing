1afd75adfd4867f5a48b9af1293904fe309595d6
FortuneCat0v0 <1142094319@qq.com>
2024-09-05 11:22:51 +0800

    Excel单独

--- Unity/Assets/Scripts/Editor/ToolEditor/ToolsEditor.cs
+++ Unity/Assets/Scripts/Editor/ToolEditor/ToolsEditor.cs
@@ -2,14 +2,14 @@
 {
     public static class ToolsEditor
     {
-        public static void ExcelExporter()
+        public static void ExcelExporter(string name = "All")
         {
 #if UNITY_EDITOR_OSX || UNITY_EDITOR_LINUX
             const string tools = "./Tool";
 #else
             const string tools = ".\\Tool.exe";
 #endif
-            ShellHelper.Run($"{tools} --AppType=ExcelExporter --Console=1", "../Bin/");
+            ShellHelper.Run($"{tools} --AppType=ExcelExporter --Console=1 --StartConfig={name}", "../Bin/");
         }
         
         public static void Proto2CS()
