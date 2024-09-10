8638fc81f69ba5dea7adccd6782a15c7fbfdd3fd
tangchunguang <394266315@qq.com>
2024-09-10 10:44:29 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Share/ConstValue.cs
+++ Unity/Assets/Scripts/Hotfix/Share/ConstValue.cs
@@ -4,7 +4,7 @@ namespace ET
     {
         public const string RouterHttpHostInter =  "127.0.0.1";
         public const string RouterHttpHostOuter =  "47.94.107.92";
-        public const int RouterHttpPort = 40300;
+        public const int RouterHttpPort = 30300;
         public const int SessionTimeoutTime = 30 * 1000;
     }
 }
