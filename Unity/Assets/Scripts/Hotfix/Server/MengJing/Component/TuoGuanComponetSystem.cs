8e836e3e987f44916dfdcb038a181177111b654f
tangchunguang <394266315@qq.com>
2024-09-19 21:02:53 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/Component/TuoGuanComponetSystem.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Component/TuoGuanComponetSystem.cs
@@ -1,11 +1,20 @@
 namespace ET.Server
 {
-    
+
     [EntitySystemOf(typeof(TuoGuanComponet))]
     [FriendOf(typeof(TuoGuanComponet))]
     public static partial class TuoGuanComponetSystem
     {
-    
+        [EntitySystem]
+        private static void Awake(this ET.Server.TuoGuanComponet self)
+        {
+
+        }
+        [EntitySystem]
+        private static void Deserialize(this ET.Server.TuoGuanComponet self)
+        {
+
+        }
     }
-    
+
 }
