7ecae0470a754fee3aafd1ec243520a4c8799122
tangchunguang <394266315@qq.com>
2024-09-19 20:48:33 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/Demo/Map/Move/C2M_PathfindingResultHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/Demo/Map/Move/C2M_PathfindingResultHandler.cs
@@ -9,7 +9,31 @@ namespace ET.Server
 
 		protected override async ETTask Run(Unit unit, C2M_PathfindingResult message)
 		{
-			unit.FindPathMoveToAsync(message.Position).Coroutine();
+
+			int petfightindex = unit.GetComponent<NumericComponentS>().GetAsInt(NumericType.PetFightIndex);
+
+			if (petfightindex == 0)
+			{
+				unit.FindPathMoveToAsync(message.Position).Coroutine();
+			}
+			else
+			{
+				PetComponentS petComponentS = unit.GetComponent<PetComponentS>();
+				if (petfightindex < 0 || petfightindex >= petComponentS.PetFightList.Count)
+				{
+					return;
+				}
+
+				RolePetInfo rolePetInfo = petComponentS.GetPetInfo(petComponentS.PetFightList[petfightindex]);
+				Unit petunit = unit.GetParent<UnitComponent>().Get(rolePetInfo.Id);
+				if (petunit == null)
+				{
+					return;
+				}
+
+				petunit.FindPathMoveToAsync(message.Position).Coroutine();
+			}
+			
 			await ETTask.CompletedTask;
 		}
 	}
--- Unity/Assets/Scripts/Hotfix/Server/MengJing/Pet/Handler/C2M_PetFightSwitchHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Pet/Handler/C2M_PetFightSwitchHandler.cs
@@ -6,6 +6,28 @@ namespace ET.Server
     {
         protected override async ETTask Run(Unit unit, C2M_PetFightSwitch request, M2C_PetFightSwitch response)
         {
+            int petfightindex = unit.GetComponent<NumericComponentS>().GetAsInt(NumericType.PetFightIndex);
+            PetComponentS petComponentS = unit.GetComponent<PetComponentS>();
+            if (petfightindex < 0 || petfightindex >= petComponentS.PetFightList.Count)
+            {
+                return;
+            }
+
+            for (int i = 0; i < petComponentS.PetFightList.Count; i++)
+            {
+                RolePetInfo rolePetInfo = petComponentS.GetPetInfo(petComponentS.PetFightList[petfightindex]);
+                Unit petunit = unit.GetParent<UnitComponent>().Get(rolePetInfo.Id);
+                
+                if ( petfightindex - 1 == i )
+                {
+                    petunit.GetComponent<AIComponent>().Stop_2();
+                }
+                else
+                {
+                    petunit.GetComponent<AIComponent>().Begin();
+                }
+            }
+
             unit.GetComponent<NumericComponentS>().ApplyValue( NumericType.PetFightIndex, request.PetFightIndex );
            
             await ETTask.CompletedTask;
