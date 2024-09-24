fb23ec4a8dfc57a8821886d4fb8ea66d00989961
FortuneCat0v0 <1142094319@qq.com>
2024-09-24 17:21:20 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgFunction/DlgFunctionSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgFunction/DlgFunctionSystem.cs
@@ -11,11 +11,8 @@ namespace ET.Client
     {
         public static void RegisterUIEvent(this DlgFunction self)
         {
-            self.View.E_CloseButton.AddListener(() =>
-            {
-                self.Root().GetComponent<UIComponent>().CloseWindow(WindowID.WindowID_Function);
-            });
-            
+            self.View.E_CloseButton.AddListener(() => { self.Root().GetComponent<UIComponent>().CloseWindow(WindowID.WindowID_Function); });
+
             self.View.E_TaskButton.AddListener(() =>
             {
                 self.Root().GetComponent<UIComponent>().ShowWindowAsync(WindowID.WindowID_Task).Coroutine();
@@ -24,10 +21,7 @@ namespace ET.Client
             {
                 self.Root().GetComponent<UIComponent>().ShowWindowAsync(WindowID.WindowID_Role).Coroutine();
             });
-            self.View.E_PetButton.AddListener(() =>
-            {
-                self.Root().GetComponent<UIComponent>().ShowWindowAsync(WindowID.WindowID_Pet).Coroutine();
-            });
+            self.View.E_PetButton.AddListener(() => { self.Root().GetComponent<UIComponent>().ShowWindowAsync(WindowID.WindowID_Pet).Coroutine(); });
             self.View.E_RoseSkillButton.AddListener(() =>
             {
                 self.Root().GetComponent<UIComponent>().ShowWindowAsync(WindowID.WindowID_Skill).Coroutine();
@@ -40,10 +34,43 @@ namespace ET.Client
             {
                 self.Root().GetComponent<UIComponent>().ShowWindowAsync(WindowID.WindowID_ChengJiu).Coroutine();
             });
+
+            ReddotViewComponent redPointComponent = self.Root().GetComponent<ReddotViewComponent>();
+            redPointComponent.RegisterReddot(ReddotType.Friend, self.Reddot_Frined);
+            redPointComponent.RegisterReddot(ReddotType.RolePoint, self.Reddot_RolePoint);
+            redPointComponent.RegisterReddot(ReddotType.SkillUp, self.Reddot_SkillUp);
         }
 
         public static void ShowWindow(this DlgFunction self, Entity contextData = null)
         {
+            ReddotComponentC reddotComponent = self.Root().GetComponent<ReddotComponentC>();
+            reddotComponent.UpdateReddont(ReddotType.FriendApply);
+            reddotComponent.UpdateReddont(ReddotType.FriendChat);
+            reddotComponent.UpdateReddont(ReddotType.RolePoint);
+            reddotComponent.UpdateReddont(ReddotType.SkillUp);
+        }
+
+        public static void BeforeUnload(this DlgFunction self)
+        {
+            ReddotViewComponent redPointComponent = self.Root().GetComponent<ReddotViewComponent>();
+            redPointComponent.UnRegisterReddot(ReddotType.Friend, self.Reddot_Frined);
+            redPointComponent.UnRegisterReddot(ReddotType.RolePoint, self.Reddot_RolePoint);
+            redPointComponent.UnRegisterReddot(ReddotType.SkillUp, self.Reddot_SkillUp);
+        }
+
+        private static void Reddot_Frined(this DlgFunction self, int num)
+        {
+            self.View.E_FriendButton.transform.Find("Reddot").gameObject.SetActive(num > 0);
+        }
+
+        private static void Reddot_RolePoint(this DlgFunction self, int num)
+        {
+            self.View.E_RoseEquipButton.transform.Find("Reddot").gameObject.SetActive(num > 0);
+        }
+
+        private static void Reddot_SkillUp(this DlgFunction self, int num)
+        {
+            self.View.E_RoseSkillButton.transform.Find("Reddot").gameObject.SetActive(num > 0);
         }
     }
 }
--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgFunction/Event/DlgFunctionEventHandler.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgFunction/Event/DlgFunctionEventHandler.cs
@@ -31,6 +31,7 @@
 
 		public void BeforeUnload(UIBaseWindow uiBaseWindow)
 		{
+			uiBaseWindow.GetComponent<DlgFunction>().BeforeUnload();
 		}
 
 	}
