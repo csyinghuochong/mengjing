f6be34b5b3e91023d64ea0c064d5de68c9de2b04
FortuneCat0v0 <1142094319@qq.com>
2024-09-27 10:40:26 +0800

    .

--- Unity/Assets/Scripts/Hotfix/Client/MengJing/Skill/SkillManagerComponentCSystem.cs
+++ Unity/Assets/Scripts/Hotfix/Client/MengJing/Skill/SkillManagerComponentCSystem.cs
@@ -34,8 +34,7 @@ namespace ET.Client
             self.FangunSkillId = int.Parse(GlobalValueConfigCategory.Instance.Get(2).Value);
 
             Unit unit = self.GetParent<Unit>();
-            self.UpdateCD = (unit.MainHero && !unit.IsSelfRobot()) ||
-                    (unit.Type == UnitType.Pet || unit.MasterId == self.Root().GetComponent<PlayerComponent>().CurrentRoleId);
+            self.UpdateCD = unit.MainHero || (unit.Type == UnitType.Pet && unit.MasterId == self.Root().GetComponent<PlayerComponent>().CurrentRoleId);
         }
 
         [EntitySystem]
