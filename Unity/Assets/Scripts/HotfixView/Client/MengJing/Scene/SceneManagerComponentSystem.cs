b60460116ec68a8c3057d56a0e3e7aa9d830e517
tangchunguang2 <2322657661@qq.com>
2024-09-30 12:43:00 +0800

    no message

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/Scene/SceneManagerComponentSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/Scene/SceneManagerComponentSystem.cs
@@ -77,7 +77,7 @@ namespace ET.Client
                     paramss = scenepath;
                     break;
                 case (int)SceneTypeEnum.CellDungeon:
-                    paramss = CellSonDungeonConfigCategory.Instance.Get(self.Root().GetComponent<MapComponent>().SonSceneId).MapID.ToString();
+                    paramss = CellDungeonConfigCategory.Instance.Get(self.Root().GetComponent<MapComponent>().SonSceneId).MapID.ToString();
                     break;
                 case (int)SceneTypeEnum.LocalDungeon:
                     paramss = DungeonConfigCategory.Instance.Get(chapterId).MapID.ToString();
