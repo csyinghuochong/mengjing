39c2e16a5d678949e455a55f905fd3137955c635
tangchunguang <394266315@qq.com>
2024-10-12 14:35:41 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/PetMelee/PetMeleeDungeonComponentSystem.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/PetMelee/PetMeleeDungeonComponentSystem.cs
@@ -3,9 +3,18 @@ namespace ET.Server
 
     [EntitySystemOf(typeof(PetMeleeDungeonComponent))]
     [FriendOf(typeof(PetMeleeDungeonComponent))]
-    public static  partial class PetMeleeDungeonComponentSystem
+    public static partial class PetMeleeDungeonComponentSystem
     {
-    
+        [EntitySystem]
+        private static void Awake(this ET.Server.PetMeleeDungeonComponent self)
+        {
+
+        }
+        [EntitySystem]
+        private static void Destroy(this ET.Server.PetMeleeDungeonComponent self)
+        {
+
+        }
     }
-    
+
 }
