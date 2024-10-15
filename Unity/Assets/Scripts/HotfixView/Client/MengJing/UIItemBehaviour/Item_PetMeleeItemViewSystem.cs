2a7c934f41724767f1d2a00620863d5bd297129a
FortuneCat0v0 <1142094319@qq.com>
2024-10-15 14:27:08 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_PetMeleeItemViewSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_PetMeleeItemViewSystem.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 
 namespace ET.Client
 {
+    [FriendOf(typeof(Scroll_Item_PetMeleeItem))]
     [EntitySystemOf(typeof(Scroll_Item_PetMeleeItem))]
     public static partial class Scroll_Item_PetMeleeItemSystem
     {
@@ -15,6 +16,17 @@ namespace ET.Client
         private static void Destroy(this Scroll_Item_PetMeleeItem self)
         {
             self.DestroyWidget();
+        }
+
+        public static void Refresh(this Scroll_Item_PetMeleeItem self, RolePetInfo rolePetInfo)
+        {
+            self.RolePetInfo = rolePetInfo;
+            self.E_ClickButton.AddListener(self.OnClick);
+        }
+
+        private static void OnClick(this Scroll_Item_PetMeleeItem self)
+        {
+            self.Root().GetComponent<UIComponent>().GetDlgLogic<DlgPetMeleeMain>().OnClickItem(self.RolePetInfo);
         }
     }
 }
