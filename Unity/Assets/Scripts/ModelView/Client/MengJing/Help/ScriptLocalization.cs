3f0d5e19e319ddd3690b47059264e701dcf0ca5b
FortuneCat0v0 <1142094319@qq.com>
2024-10-22 13:50:18 +0800

    .

--- Unity/Assets/Scripts/ModelView/Client/MengJing/Help/ScriptLocalization.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/Help/ScriptLocalization.cs
@@ -3,6 +3,7 @@ using I2.Loc;
 
 namespace ET.Client
 {
+   // 该脚本由工具自动生成，请勿修改
 	public static class ScriptLocalization
 	{
 
@@ -10,6 +11,8 @@ namespace ET.Client
 		public static string Test 		{ get{ return LocalizationManager.GetTranslation ("Test"); } }
         [StaticField]
 		public static string UnityWhite 		{ get{ return LocalizationManager.GetTranslation ("UnityWhite"); } }
+        [StaticField]
+		public static string 你好_123 		{ get{ return LocalizationManager.GetTranslation ("你好 123"); } }
         [StaticField]
 		public static string 加油 		{ get{ return LocalizationManager.GetTranslation ("加油"); } }
         [StaticField]
@@ -23,6 +26,7 @@ namespace ET.Client
 
 		public const string Test = "Test";
 		public const string UnityWhite = "UnityWhite";
+		public const string 你好_123 = "你好 123";
 		public const string 加油 = "加油";
 		public const string 成功 = "成功";
 		public const string 测试 = "测试";
