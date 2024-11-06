934b6b991e49cce752a3340f84c6a095c6699cd7
FortuneCat0v0 <1142094319@qq.com>
2024-11-06 15:20:24 +0800

    写个工具，检测一下GitMengJing\Unity\Assets\Bundles\Scenes这个目录下的所有场景，是否包含AdditiveHide这个节点并打印。

---
+++ Unity/Assets/Scripts/Editor/Custom/SceneChecker.cs
@@ -0,0 +1,59 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ET.Client
+{
+    public class SceneChecker : EditorWindow
+    {
+        [MenuItem("Tools/检测场景包含 AdditiveHide节点")]
+        public static void CheckScenesForAdditiveHideAndListAllNodes()
+        {
+            string scenesDirectory = "Assets/Bundles/Scenes";
+            string[] sceneFiles = Directory.GetFiles(scenesDirectory, "*.unity", SearchOption.AllDirectories);
+
+            foreach (string scenePath in sceneFiles)
+            {
+                SceneAsset sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(scenePath);
+                if (sceneAsset == null)
+                {
+                    Log.Warning($"无法加载场景: {scenePath}");
+                    continue;
+                }
+
+                EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+
+                GameObject additiveHideObject = GameObject.Find("AdditiveHide");
+                if (additiveHideObject != null)
+                {
+                    string objectPath = GetFullPath(additiveHideObject);
+                    Log.Debug($"场景 '{sceneAsset.name}' 包含 'AdditiveHide' 物体，路径为: {objectPath}");
+                }
+                else
+                {
+                    Log.Debug($"场景 '{sceneAsset.name}' 中不存在 'AdditiveHide' 物体.");
+                }
+            }
+
+            Log.Debug("所有场景检查完成！");
+        }
+
+        // 递归获取物体的完整路径
+        private static string GetFullPath(GameObject obj)
+        {
+            string path = obj.name;
+            Transform current = obj.transform;
+
+            while (current.parent != null)
+            {
+                current = current.parent;
+                path = current.name + "/" + path;
+            }
+
+            return path;
+        }
+    }
+}
