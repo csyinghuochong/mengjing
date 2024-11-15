57f78931899929376393b35b79af85e23aaa26a3
tangchunguang <394266315@qq.com>
2024-11-15 16:10:36 +0800

    no message

---
+++ Unity/Assets/Scripts/Hotfix/Client/MengJing/Skill/Action/Skill_XuanZhuan_Attack_2.cs
@@ -0,0 +1,51 @@
+using Unity.Mathematics;
+
+namespace ET.Client
+{
+    
+    public class Skill_XuanZhuan_Attack_2 : Skill_Action_Common
+    {
+        public override void OnInit(SkillC skils, Unit theUnitFrom)
+        {
+            skils.BaseOnInit(skils.SkillInfo, theUnitFrom);
+
+            this.OnExecute(skils);
+        }
+
+        public override void OnExecute(SkillC skils)
+        {
+            int angle = skils.SkillInfo.TargetAngle;
+            skils.PlaySkillEffects( skils.TheUnitFrom.Position, angle );
+
+            this.OnUpdate(skils);
+        }
+
+        public override void OnUpdate(SkillC skils)
+        {
+            long serverNow = TimeHelper.ServerNow();
+            //根据技能效果延迟触发伤害
+            if (serverNow < skils.SkillExcuteHurtTime)
+            {
+                return;
+            }
+            int angle = (int)(skils.TheUnitFrom.Rotation.value).y;
+            for (int i = 0; i < skils.EffectInstanceId.Count; i++)
+            {
+                EventSystem.Instance.Publish(skils.Root(), new SkillEffectMove()
+                {
+                    Postion = skils.TargetPosition,
+                    Unit = skils.TheUnitFrom,
+                    Angle = angle,
+                    EffectInstanceId = skils.EffectInstanceId[0],
+                });
+            }
+
+            skils.BaseOnUpdate();
+        }
+
+        public override void OnFinished(SkillC skils)
+        {
+            skils.EndSkillEffect();
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Skill/Action/Skill_XuanZhuan_Attack_2.cs
@@ -0,0 +1,55 @@
+using Unity.Mathematics;
+
+namespace ET.Server
+{
+    //旋转攻击
+    public class Skill_XuanZhuan_Attack_2 : SkillHandlerS
+    {
+
+        public override void OnInit(SkillS skillS, Unit theUnitFrom)
+        {
+            skillS.BaseOnInit(skillS.SkillInfo, theUnitFrom);
+
+            skillS.SkillTriggerInvelTime = 1000;
+            skillS.SkillTriggerLastTime = TimeHelper.ServerNow();
+            OnExecute(skillS);
+        }
+
+        public override void OnExecute(SkillS skillS)
+        {
+            skillS.InitSelfBuff();
+            this.OnUpdate(skillS, 0);
+        }
+
+        public override void OnUpdate(SkillS skillS, int updateMode)
+        {
+            long serverNow = TimeHelper.ServerNow();
+            //根据技能效果延迟触发伤害
+            if (serverNow < skillS.SkillExcuteHurtTime)
+            {
+                return;
+            }
+            //根据技能存在时间设置其结束状态
+            if (serverNow > skillS.SkillEndTime)
+            {
+                skillS.SetSkillState(SkillState.Finished);
+                return;
+            }
+
+            for (int i = 0; i < skillS.ICheckShape.Count; i++)
+            {
+                // (int)Quaternion.QuaternionToEuler().y;
+                // (this.ICheckShape[i] as Rectangle).s_forward = 
+                (skillS.ICheckShape[i] as Rectangle).s_forward = math.forward(skillS.TheUnitFrom.Rotation);
+            }
+            
+            skillS.ExcuteSkillAction();
+            skillS.CheckChiXuHurt();
+        }
+
+        public override void OnFinished(SkillS skillS)
+        {
+            skillS.Clear();
+        }
+    }
+}
