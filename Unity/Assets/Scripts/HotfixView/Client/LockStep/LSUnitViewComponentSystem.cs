ccdb3abd165ae43218ab0988e56addd608031b48
tangchunguang <394266315@qq.com>
2024-11-20 17:27:36 +0800

    no message

--- Unity/Assets/Scripts/HotfixView/Client/LockStep/LSUnitViewComponentSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/LockStep/LSUnitViewComponentSystem.cs
@@ -19,23 +19,7 @@ namespace ET.Client
 
         public static async ETTask InitAsync(this LSUnitViewComponent self)
         {
-            Room room = self.Room();
-            LSUnitComponent lsUnitComponent = room.LSWorld.GetComponent<LSUnitComponent>();
-            Scene root = self.Root();
-            foreach (long playerId in room.PlayerIds)
-            {
-                LSUnit lsUnit = lsUnitComponent.GetChild<LSUnit>(playerId);
-                string assetsName = $"Assets/Bundles/Unit/Unit.prefab";
-                GameObject bundleGameObject = await room.GetComponent<ResourcesLoaderComponent>().LoadAssetAsync<GameObject>(assetsName);
-                GameObject prefab = bundleGameObject.Get<GameObject>("Skeleton");
-
-                GlobalComponent globalComponent = root.GetComponent<GlobalComponent>();
-                GameObject unitGo = UnityEngine.Object.Instantiate(prefab, globalComponent.Unit, true);
-                unitGo.transform.position = lsUnit.Position.ToVector();
-
-                LSUnitView lsUnitView = self.AddChildWithId<LSUnitView, GameObject>(lsUnit.Id, unitGo);
-                lsUnitView.AddComponent<LSAnimatorComponent>();
-            }
+            await ETTask.CompletedTask;
         }
     }
 }
