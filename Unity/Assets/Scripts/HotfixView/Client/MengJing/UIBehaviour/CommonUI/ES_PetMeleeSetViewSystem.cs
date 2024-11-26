704c5d9ddc86593b56b79b0b6eb937895a676b3e
FortuneCat0v0 <1142094319@qq.com>
2024-11-26 19:24:52 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UIBehaviour/CommonUI/ES_PetMeleeSetViewSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIBehaviour/CommonUI/ES_PetMeleeSetViewSystem.cs
@@ -1,24 +1,51 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+
 namespace ET.Client
 {
-	[EntitySystemOf(typeof(ES_PetMeleeSet))]
-	[FriendOfAttribute(typeof(ES_PetMeleeSet))]
-	public static partial class ES_PetMeleeSetSystem 
-	{
-		[EntitySystem]
-		private static void Awake(this ES_PetMeleeSet self,Transform transform)
-		{
-			self.uiTransform = transform;
-		}
+    [EntitySystemOf(typeof(ES_PetMeleeSet))]
+    [FriendOfAttribute(typeof(ES_PetMeleeSet))]
+    public static partial class ES_PetMeleeSetSystem
+    {
+        [EntitySystem]
+        private static void Awake(this ES_PetMeleeSet self, Transform transform)
+        {
+            self.E_PlanSetToggleGroup.AddListener((index) => { self.OnPlanSet(index).Coroutine(); });
 
-		[EntitySystem]
-		private static void Destroy(this ES_PetMeleeSet self)
-		{
-			self.DestroyWidget();
-		}
-	}
+            self.E_SetMainButton.AddListener(self.OnSetMain);
+            self.E_SetAssistButton.AddListener(self.OnSetAssist);
+            self.E_SetMagicButton.AddListener(self.SetMagic);
 
+            self.uiTransform = transform;
+        }
 
+        [EntitySystem]
+        private static void Destroy(this ES_PetMeleeSet self)
+        {
+            self.DestroyWidget();
+        }
+
+        private static async ETTask OnPlanSet(this ES_PetMeleeSet self, int index)
+        {
+            PetComponentC petComponentC = self.Root().GetComponent<PetComponentC>();
+
+            if (petComponentC.PetFightPlan != index)
+            {
+            }
+
+            await ETTask.CompletedTask;
+        }
+
+        private static void OnSetMain(this ES_PetMeleeSet self)
+        {
+        }
+
+        private static void OnSetAssist(this ES_PetMeleeSet self)
+        {
+        }
+
+        private static void SetMagic(this ES_PetMeleeSet self)
+        {
+        }
+    }
 }
--- Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/CommonUI/ES_PetMeleeSet.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/CommonUI/ES_PetMeleeSet.cs
@@ -24,6 +24,142 @@ namespace ET.Client
      		}
      	}
 
+		public UnityEngine.UI.Button E_SetMainButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_SetMainButton == null )
+     			{
+		    		this.m_E_SetMainButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"Left/E_SetMain");
+     			}
+     			return this.m_E_SetMainButton;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_SetMainImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_SetMainImage == null )
+     			{
+		    		this.m_E_SetMainImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"Left/E_SetMain");
+     			}
+     			return this.m_E_SetMainImage;
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_SetAssistButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_SetAssistButton == null )
+     			{
+		    		this.m_E_SetAssistButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"Left/E_SetAssist");
+     			}
+     			return this.m_E_SetAssistButton;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_SetAssistImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_SetAssistImage == null )
+     			{
+		    		this.m_E_SetAssistImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"Left/E_SetAssist");
+     			}
+     			return this.m_E_SetAssistImage;
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_PetBarSetIconButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_PetBarSetIconButton == null )
+     			{
+		    		this.m_E_PetBarSetIconButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"Left/PetMask/E_PetBarSetIcon");
+     			}
+     			return this.m_E_PetBarSetIconButton;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_PetBarSetIconImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_PetBarSetIconImage == null )
+     			{
+		    		this.m_E_PetBarSetIconImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"Left/PetMask/E_PetBarSetIcon");
+     			}
+     			return this.m_E_PetBarSetIconImage;
+     		}
+     	}
+
+		public UnityEngine.UI.Button E_SetMagicButton
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_SetMagicButton == null )
+     			{
+		    		this.m_E_SetMagicButton = UIFindHelper.FindDeepChild<UnityEngine.UI.Button>(this.uiTransform.gameObject,"Left/E_SetMagic");
+     			}
+     			return this.m_E_SetMagicButton;
+     		}
+     	}
+
+		public UnityEngine.UI.Image E_SetMagicImage
+     	{
+     		get
+     		{
+     			if (this.uiTransform == null)
+     			{
+     				Log.Error("uiTransform is null.");
+     				return null;
+     			}
+     			if( this.m_E_SetMagicImage == null )
+     			{
+		    		this.m_E_SetMagicImage = UIFindHelper.FindDeepChild<UnityEngine.UI.Image>(this.uiTransform.gameObject,"Left/E_SetMagic");
+     			}
+     			return this.m_E_SetMagicImage;
+     		}
+     	}
+
 		    public Transform UITransform
          {
      	    get
@@ -39,10 +175,26 @@ namespace ET.Client
 		public void DestroyWidget()
 		{
 			this.m_E_PlanSetToggleGroup = null;
+			this.m_E_SetMainButton = null;
+			this.m_E_SetMainImage = null;
+			this.m_E_SetAssistButton = null;
+			this.m_E_SetAssistImage = null;
+			this.m_E_PetBarSetIconButton = null;
+			this.m_E_PetBarSetIconImage = null;
+			this.m_E_SetMagicButton = null;
+			this.m_E_SetMagicImage = null;
 			this.uiTransform = null;
 		}
 
 		private UnityEngine.UI.ToggleGroup m_E_PlanSetToggleGroup = null;
+		private UnityEngine.UI.Button m_E_SetMainButton = null;
+		private UnityEngine.UI.Image m_E_SetMainImage = null;
+		private UnityEngine.UI.Button m_E_SetAssistButton = null;
+		private UnityEngine.UI.Image m_E_SetAssistImage = null;
+		private UnityEngine.UI.Button m_E_PetBarSetIconButton = null;
+		private UnityEngine.UI.Image m_E_PetBarSetIconImage = null;
+		private UnityEngine.UI.Button m_E_SetMagicButton = null;
+		private UnityEngine.UI.Image m_E_SetMagicImage = null;
 		public Transform uiTransform = null;
 	}
 }
