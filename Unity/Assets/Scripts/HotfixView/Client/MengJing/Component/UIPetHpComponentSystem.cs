20eb383398d11be8578c97d469d2610529b49fa2
tangchunguang <394266315@qq.com>
2024-11-27 15:23:14 +0800

    no message

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/Component/UIPetHpComponentSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/Component/UIPetHpComponentSystem.cs
@@ -109,7 +109,7 @@ namespace ET.Client
             bool canAttack = mainUnit.IsCanAttackUnit(unit);
             self.Img_HpValue = rc.Get<GameObject>("Img_HpValue");
 
-            string imageHp = canAttack ?ConfigData.UI_pro_4_2  : ConfigData.UI_pro_3_4 ;
+            string imageHp = canAttack ?  ConfigData.UI_pro_3_4 :ConfigData.UI_pro_4_2 ;
             Sprite sp = rc.Get<GameObject>(imageHp).GetComponent<Image>().sprite;
             self.Img_HpValue.GetComponent<Image>().sprite = sp;
 
