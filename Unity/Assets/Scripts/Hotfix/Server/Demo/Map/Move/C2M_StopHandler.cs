0bb73407bd52e78f4ecf0a3fc0de5539d7c62807
tangchunguang <394266315@qq.com>
2024-11-28 18:03:39 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/Demo/Map/Move/C2M_StopHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/Demo/Map/Move/C2M_StopHandler.cs
@@ -5,23 +5,7 @@
     {
         protected override async ETTask Run(Unit unit, C2M_Stop message)
         {
-            int petfightindex = unit.GetComponent<NumericComponentS>().GetAsInt(NumericType.PetFightIndex);
-
-            if (petfightindex == 0)
-            {
-                unit.Stop(0);
-            }
-            else
-            {
-                PetComponentS petComponentS = unit.GetComponent<PetComponentS>();
-                Unit petunit = petComponentS.GetFightPetByIndex(petfightindex);
-                if (petunit == null)
-                {
-                    return;
-                }
-                
-                petunit.Stop(0);
-            }
+            unit.Stop(0);
             
             await ETTask.CompletedTask;
         }
