e2a416b7506bb41398d762b150081ac490d8a4fb
tangchunguang <394266315@qq.com>
2024-11-29 16:50:02 +0800

    no message

--- Unity/Assets/Scripts/Core/Helper/MD5Helper.cs
+++ Unity/Assets/Scripts/Core/Helper/MD5Helper.cs
@@ -3,8 +3,11 @@ using System.Security.Cryptography;
 
 namespace ET
 {
+	
 	public static class MD5Helper
 	{
+		
+
 		public static string FileMD5(string filePath)
 		{
 			byte[] retVal;
@@ -13,6 +16,16 @@ namespace ET
 	            MD5 md5 = MD5.Create();
 				retVal = md5.ComputeHash(file);
 			}
+			return retVal.ToHex("x2");
+		}
+		
+		public static string GetMd5Hash(string input)
+		{
+			byte[] retVal;
+			MD5 md5 = MD5.Create();
+        
+			retVal =  md5.ComputeHash(input.ToByteArray());
+			md5.Dispose();
 			return retVal.ToHex("x2");
 		}
 	}
