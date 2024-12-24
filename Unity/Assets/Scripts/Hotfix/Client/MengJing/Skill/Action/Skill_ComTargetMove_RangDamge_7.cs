8b52894fbbb86e28a1342eff1dfaf3ce271fcd2b
tangchunguang <394266315@qq.com>
2024-12-24 10:59:17 +0800

    no message

---
+++ Unity/Assets/Scripts/Hotfix/Client/MengJing/Skill/Action/Skill_ComTargetMove_RangDamge_7.cs
@@ -0,0 +1,31 @@
+namespace ET.Client
+{
+
+    /// <summary>
+    /// 立即对目标释放一个燃烧种子造成120%伤害,并持续燃烧对附近单位每秒造成70%伤害,持续5秒
+    /// </summary>
+    public class Skill_ComTargetMove_RangDamge_7: SkillHandlerC
+    {
+        
+        public override void OnInit(SkillC skils, Unit theUnitFrom)
+        {
+            skils.BaseOnInit(skils.SkillInfo, theUnitFrom);
+            this.OnExecute(skils);
+        }
+
+        public override void OnExecute(SkillC skils)
+        {
+            skils.OnShowSkillIndicator(skils.SkillInfo);
+            this.OnUpdate(skils);
+        }
+
+        public override void OnUpdate(SkillC skils)
+        {
+            skils.BaseOnUpdate();
+        }
+
+        public override void OnFinished(SkillC skils)
+        {
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Skill/Action/Skill_ComTargetMove_RangDamge_7.cs
@@ -0,0 +1,67 @@
+using Unity.Mathematics;
+
+namespace ET.Server
+{
+    /// <summary>
+    /// 立即对目标释放一个燃烧种子造成120%伤害,并持续燃烧对附近单位每秒造成70%伤害,持续5秒
+    /// </summary>
+    public class Skill_ComTargetMove_RangDamge_7 : SkillHandlerS
+    {
+        public override void OnInit(SkillS skillS, Unit theUnitFrom)
+        {
+            skillS.BaseOnInit(skillS.SkillInfo, theUnitFrom);
+
+            string[] paraminfos = skillS.SkillConf.GameObjectParameter.Split(';');
+            skillS.MoveTime = (long)(float.Parse(paraminfos[0]) * 1000);
+            skillS.SkillTriggerInvelTime = (long)(float.Parse(paraminfos[1]) * 1000);
+            skillS.IsStop = int.Parse(paraminfos[2]);
+
+            skillS.SkillExcuteNum = 1;
+        }
+
+        public override void OnExecute(SkillS skillS)
+        {
+            skillS.InitSelfBuff();
+            this.OnUpdate(skillS, 0);
+        }
+
+        public override void OnUpdate(SkillS skillS, int updateMode)
+        {
+            this.CreateBullet(skillS);
+            if (TimeHelper.ServerNow() > skillS.SkillEndTime)
+            {
+                skillS.SetSkillState(SkillState.Finished);
+                return;
+            }
+        }
+
+        public override void OnFinished(SkillS skillS)
+        {
+            skillS.Clear();
+        }
+
+        public void CreateBullet(SkillS skillS)
+        {
+            if (TimeHelper.ServerNow() < skillS.SkillExcuteHurtTime)
+            {
+                return;
+            }
+
+            if (skillS.SkillExcuteNum <= 0)
+            {
+                return;
+            }
+
+            Unit unit = UnitFactory.CreateBullet(skillS.TheUnitFrom.Scene(), skillS.TheUnitFrom.Id, skillS.SkillConf.Id, 0,
+                skillS.TheUnitFrom.Position,
+                new CreateMonsterInfo());
+            unit.AddComponent<RoleBullet6Componnet>().OnBaseBulletInit(skillS, skillS.TheUnitFrom.Id, skillS.IsStop);
+            float3 sourcePoint = skillS.TheUnitFrom.Position;
+            quaternion rotation = quaternion.Euler(0, math.radians(skillS.SkillInfo.TargetAngle), 0);
+            float3 TargetPoint = sourcePoint +
+                    math.mul(rotation, new float3(0, 0, 1)) * skillS.MoveTime * (float)skillS.SkillConf.SkillMoveSpeed * 0.001f;
+            unit.BulletMoveToAsync(TargetPoint).Coroutine();
+            skillS.SkillExcuteNum--;
+        }
+    }
+}
