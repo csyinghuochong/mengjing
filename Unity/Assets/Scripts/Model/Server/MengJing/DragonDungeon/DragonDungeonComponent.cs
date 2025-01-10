03c5c8be76858ec6990aa92e223714a250c216be
tangchunguang <394266315@qq.com>
2025-01-10 19:23:08 +0800

    no message

---
+++ Unity/Assets/Scripts/Model/Server/MengJing/DragonDungeon/DragonDungeonComponent.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Unity.Mathematics;
+
+namespace ET.Server
+{
+    
+    [ComponentOf(typeof(Scene))]
+    public class DragonDungeonComponent : Entity, IAwake, IDestroy
+    {
+        public long Timer;
+        public int FubenType { get; set; }
+        public long EnterTime { get; set; }
+        
+        public long TeamId { get; set; }
+        
+        public Dictionary<long, TeamPlayerInfo> TeamPlayers  { get; set; }= new Dictionary<long, TeamPlayerInfo>();
+
+        public List<int> BoxReward  { get; set; }= new List<int>();
+        public List<int> KillBossList  { get; set; }= new List<int>();
+        
+        public List<TeamDropItem> TeamDropItems { get; set; } = new List<TeamDropItem>();
+        public Dictionary<long, long> ItemFlags  { get; set; }= new Dictionary<long, long>();
+
+        public M2C_TeamPickMessage m2C_TeamPickMessage { get; set; } = M2C_TeamPickMessage.Create();
+
+        public float3 BossDeadPosition  { get; set; }= float3.zero;
+    }
+    
+}
