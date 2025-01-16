a7d6e3cc5f21f1997d2b9506741e731ed853790f
DESKTOP-0DOPE9C\Administrator <hesongnian123@vip.qq.com>
2025-01-16 20:00:41 +0800

    更新配置

--- Unity/Assets/Scripts/Model/Share/MengJing/ConfigData/ConfigData.cs
+++ Unity/Assets/Scripts/Model/Share/MengJing/ConfigData/ConfigData.cs
@@ -1994,7 +1994,7 @@ namespace ET
         /// 测试 魔法卡技能，目前由主角释放，配置技能的释放范围要大些
         /// </summary>
         [StaticField]
-        public static List<int> PetMeleeMagicTest = new() { 1001 };
+        public static List<int> PetMeleeMagicTest = new() { 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014, 1015, 1016, 1017 };
 
         /// <summary>
         /// 手持多少张牌
