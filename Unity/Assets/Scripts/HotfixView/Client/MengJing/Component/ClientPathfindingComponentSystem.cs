4639faf228229deb2c82176b2254a4942f6df9cd
tangchunguang <394266315@qq.com>
2025-02-19 15:43:48 +0800

    no message

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/Component/ClientPathfindingComponentSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/Component/ClientPathfindingComponentSystem.cs
@@ -22,8 +22,8 @@ namespace ET.Client
             TextAsset textAsset = self.Root().GetComponent<ResourcesLoaderComponent>().LoadAssetSync<TextAsset>(ABPathHelper.GetRecastPath(name));
             if(textAsset == null)
             {
-                FlyTipComponent.Instance.ShowFlyTip(zstring.Format("加载寻路数据失败： {0}", name));
-                Log.Error(zstring.Format("加载寻路数据失败： {0}", name));
+                FlyTipComponent.Instance.ShowFlyTip(string.Format("加载寻路数据失败： {0}", name));
+                Log.Error(string.Format("加载寻路数据失败： {0}", name));
                 return;
             }
             
