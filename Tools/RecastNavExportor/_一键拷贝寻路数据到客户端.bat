1947af518e78a7e3d51c4cd4058af6bb8cce283f
FortuneCat0v0 <1142094319@qq.com>
2025-02-20 16:37:38 +0800

    .

--- Tools/RecastNavExportor/_一键拷贝寻路数据到客户端.bat
+++ Tools/RecastNavExportor/_一键拷贝寻路数据到客户端.bat
@@ -12,16 +12,12 @@ if not exist "%TARGET_DIR%" (
     mkdir "%TARGET_DIR%"
 )
 
-rem 清空目标目录中的所有文件
-del /q "%TARGET_DIR%\*"
-
 rem 遍历当前目录下的所有 .bin 文件
 for %%f in ("%SOURCE_DIR%*.bin") do (
     rem 获取文件名（不带扩展名）
     set FILENAME=%%~nf
-    rem 复制文件并修改扩展名为 .bytes
-    copy "%%f" "%TARGET_DIR%\!FILENAME!.bytes"
+    rem 复制文件并修改扩展名为 .bytes，强制替换已存在文件
+    copy /y "%%f" "%TARGET_DIR%\!FILENAME!.bytes"
 )
 
 pause
-
