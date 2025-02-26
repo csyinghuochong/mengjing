fded434615c95494a8701dc6ca9addfe649cb0e5
tangchunguang <394266315@qq.com>
2025-02-26 17:22:51 +0800

    no message

--- Unity/Assets/Scripts/Model/Share/Recast/ET6PathfindingComponent.cs
+++ Unity/Assets/Scripts/Model/Share/Recast/ET6PathfindingComponent.cs
@@ -2,18 +2,22 @@ using System;
 
 namespace ET.Recast
 {
-    
+    [EnableClass]
     public class NoMemoryCheck: Attribute
     {
     }
     
+    [EnableClass]
     /// <summary>
     /// 同一块地图可能有多种寻路数据，玩家可以随时切换，怪物也可能跟玩家的寻路不一样，寻路组件应该挂在Unit上
     /// </summary>
     public class ET6PathfindingComponent: Entity, IAwake<int>, IDestroy
     {
+        
+        [StaticField]
         public static int FindRandomNavPosMaxRadius = 15000;  // 随机找寻路点的最大半径
 
+        [StaticField]
         public static float[] extents = {15, 10, 15};
 
         public string Name;
