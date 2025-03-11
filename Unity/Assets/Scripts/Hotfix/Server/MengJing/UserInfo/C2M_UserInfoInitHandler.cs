1b946fe58cd062f7904fd61b4ae3e7cb46082281
FortuneCat0v0 <1142094319@qq.com>
2025-03-11 19:26:30 +0800

    .

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/UserInfo/C2M_UserInfoInitHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/UserInfo/C2M_UserInfoInitHandler.cs
@@ -7,6 +7,10 @@
         protected override async ETTask Run(Unit unit, C2M_UserInfoInitRequest request, M2C_UserInfoInitResponse response)
         {
             response.UserInfoProto = unit.GetComponent<UserInfoComponentS>().UserInfo.ToMessage();
+            response.ReddontList =  unit.GetComponent<ReddotComponentS>().ReddontList;
+            response.TreasureInfo = unit.GetComponent<ShoujiComponentS>().TreasureInfo;
+            response.ShouJiChapterInfos = unit.GetComponent<ShoujiComponentS>().ShouJiChapterInfos;
+            response.TitleList = unit.GetComponent<TitleComponentS>().TitleList;
             await ETTask.CompletedTask;
         }
     }
