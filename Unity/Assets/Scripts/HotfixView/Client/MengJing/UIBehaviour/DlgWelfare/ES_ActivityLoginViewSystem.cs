ed5967e19052fd390acd2441d516e0e32add2a0c
FortuneCat0v0 <1142094319@qq.com>
2025-03-14 17:27:14 +0800

    7天登录改成这种布局，Icon展示第一个道具的Icon即可

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UIBehaviour/DlgWelfare/ES_ActivityLoginViewSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIBehaviour/DlgWelfare/ES_ActivityLoginViewSystem.cs
@@ -1,18 +1,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ET.Client
 {
-    [EntitySystemOf(typeof (ES_ActivityLogin))]
-    [FriendOfAttribute(typeof (ES_ActivityLogin))]
+    [EntitySystemOf(typeof(ES_ActivityLogin))]
+    [FriendOfAttribute(typeof(ES_ActivityLogin))]
     public static partial class ES_ActivityLoginSystem
     {
         [EntitySystem]
         private static void Awake(this ES_ActivityLogin self, Transform transform)
         {
             self.uiTransform = transform;
-            self.E_ActivityLoginItemsLoopVerticalScrollRect.AddItemRefreshListener(self.OnActivityLoginItemsRefresh);
+            self.OnUpdateUI();
         }
 
         [EntitySystem]
@@ -21,31 +22,108 @@ namespace ET.Client
             self.DestroyWidget();
         }
 
-        private static void OnActivityLoginItemsRefresh(this ES_ActivityLogin self, Transform transform, int index)
-        {
-            ActivityComponentC activityComponent = self.Root().GetComponent<ActivityComponentC>();
-            Scroll_Item_ActivityLoginItem scrollItemActivityLoginItem = self.ScrollItemActivityLoginItems[index].BindTrans(transform);
-            scrollItemActivityLoginItem.OnUpdateUI(self.ShowActivityConfigs[index]);
-            scrollItemActivityLoginItem.SetReceived(activityComponent.ActivityReceiveIds.Contains(self.ShowActivityConfigs[index].Id));
-        }
-
         public static void OnUpdateUI(this ES_ActivityLogin self)
         {
+            ActivityComponentC activityComponent = self.Root().GetComponent<ActivityComponentC>();
             List<ActivityConfig> activityConfigs = ActivityConfigCategory.Instance.GetAll().Values.ToList();
 
-            self.ShowActivityConfigs.Clear();
+            List<ActivityConfig> showConfigs = new();
             for (int i = 0; i < activityConfigs.Count; i++)
             {
-                if (activityConfigs[i].ActivityType != 31)
+                if (activityConfigs[i].ActivityType != (int)ActivityEnum.Type_31)
                 {
                     continue;
                 }
 
-                self.ShowActivityConfigs.Add(activityConfigs[i]);
+                showConfigs.Add(activityConfigs[i]);
             }
 
-            self.AddUIScrollItems(ref self.ScrollItemActivityLoginItems, self.ShowActivityConfigs.Count);
-            self.E_ActivityLoginItemsLoopVerticalScrollRect.SetVisible(true, self.ShowActivityConfigs.Count);
+            ResourcesLoaderComponent resourcesLoaderComponent = self.Root().GetComponent<ResourcesLoaderComponent>();
+            for (int i = 0; i < showConfigs.Count; i++)
+            {
+                ActivityConfig activityConfig = showConfigs[i];
+
+                Transform itemTransform = null;
+                using (zstring.Block())
+                {
+                    itemTransform = self.uiTransform.Find(zstring.Format("ActivityLoginItem_{0}", i + 1));
+                    
+                    itemTransform.Find("Text_Day").GetComponent<Text>().text = zstring.Format("0{0}", i + 1);
+
+                    string[] items = activityConfig.Par_3.Split('@');
+                    string[] item = items[0].Split(';');
+                    ItemConfig itemConfig = ItemConfigCategory.Instance.Get(int.Parse(item[0]));
+                    itemTransform.Find("Img_ItemIcon").GetComponent<Image>().sprite = resourcesLoaderComponent.LoadAssetSync<Sprite>(ABPathHelper.GetAtlasPath_2(ABAtlasTypes.ItemIcon, itemConfig.Icon));
+                    itemTransform.Find("Text_ItemName").GetComponent<Text>().text = zstring.Format("{0} x {1}", itemConfig.ItemName, int.Parse(item[1]));
+                    itemTransform.Find("Text_ItemName").GetComponent<Text>().color = FunctionUI.QualityReturnColorDi(itemConfig.ItemQuality);
+                }
+
+                bool received = activityComponent.ActivityReceiveIds.Contains(activityConfig.Id);
+                itemTransform.Find("Btn_Receive").gameObject.SetActive(!received);
+                itemTransform.Find("Img_Received").gameObject.SetActive(received);
+                itemTransform.Find("Text_State").GetComponent<Text>().text = received ? "已领取" : "待签到";
+
+                itemTransform.Find("Btn_Receive").GetComponent<Button>().AddListener(() => { self.OnBtn_Receive(activityConfig).Coroutine(); });
+            }
+        }
+        private static async ETTask OnBtn_Receive(this ES_ActivityLogin self, ActivityConfig activityConfig)
+        {
+            UserInfo userInfo = self.Root().GetComponent<UserInfoComponentC>().UserInfo;
+            if (userInfo.Lv < 10)
+            {
+                FlyTipComponent.Instance.ShowFlyTip(LanguageComponent.Instance.LoadLocalization("10级才能领取！"));
+                return;
+            }
+
+            if (!self.CanReceive(activityConfig.Id))
+            {
+                FlyTipComponent.Instance.ShowFlyTip(LanguageComponent.Instance.LoadLocalization("未达到领取条件！"));
+                return;
+            }
+
+            ActivityComponentC activityComponent = self.Root().GetComponent<ActivityComponentC>();
+            if (CommonHelp.GetDayByTime(activityComponent.LastLoginTime) == CommonHelp.GetDayByTime(TimeHelper.ServerNow()))
+            {
+                FlyTipComponent.Instance.ShowFlyTip(LanguageComponent.Instance.LoadLocalization("今天的奖励已领取"));
+                return;
+            }
+
+            int errorCode = await ActivityNetHelper.ActivityReceive(self.Root(), activityConfig.ActivityType, activityConfig.Id);
+            if (errorCode == ErrorCode.ERR_Success)
+            {
+                self.OnUpdateUI();
+            }
+
+            self.Root().GetComponent<ReddotComponentC>().UpdateReddont(ReddotType.WelfareLogin);
+        }
+
+        private static bool CanReceive(this ES_ActivityLogin self, int activityId)
+        {
+            List<int> allIds = new List<int>();
+            ActivityComponentC activityComponent = self.Root().GetComponent<ActivityComponentC>();
+            List<ActivityConfig> activityConfigs = ActivityConfigCategory.Instance.GetAll().Values.ToList();
+            for (int i = 0; i < activityConfigs.Count; i++)
+            {
+                if (activityConfigs[i].ActivityType != (int)ActivityEnum.Type_31)
+                {
+                    continue;
+                }
+
+                if (activityConfigs[i].Id < activityId)
+                {
+                    allIds.Add(activityConfigs[i].Id);
+                }
+            }
+
+            for (int i = 0; i < allIds.Count; i++)
+            {
+                if (!activityComponent.ActivityReceiveIds.Contains(allIds[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
--- Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/DlgWelfare/ES_ActivityLogin.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/DlgWelfare/ES_ActivityLogin.cs
@@ -1,33 +1,12 @@
-﻿using System.Collections.Generic;
+﻿
 using UnityEngine;
 using UnityEngine.UI;
-
 namespace ET.Client
 {
 	[ChildOf]
 	[EnableMethod]
-	public  class ES_ActivityLogin : Entity,IAwake<Transform>,IDestroy,IUILogic
+	public  class ES_ActivityLogin : Entity,ET.IAwake<UnityEngine.Transform>,IDestroy 
 	{
-		public List<ActivityConfig> ShowActivityConfigs = new();
-		public Dictionary<int, EntityRef<Scroll_Item_ActivityLoginItem>> ScrollItemActivityLoginItems;
-		
-		public LoopVerticalScrollRect E_ActivityLoginItemsLoopVerticalScrollRect
-     	{
-     		get
-     		{
-     			if (this.uiTransform == null)
-     			{
-     				Log.Error("uiTransform is null.");
-     				return null;
-     			}
-     			if( this.m_E_ActivityLoginItemsLoopVerticalScrollRect == null )
-     			{
-		    		this.m_E_ActivityLoginItemsLoopVerticalScrollRect = UIFindHelper.FindDeepChild<LoopVerticalScrollRect>(this.uiTransform.gameObject,"E_ActivityLoginItems");
-     			}
-     			return this.m_E_ActivityLoginItemsLoopVerticalScrollRect;
-     		}
-     	}
-
 		    public Transform UITransform
          {
      	    get
@@ -42,11 +21,9 @@ namespace ET.Client
 
 		public void DestroyWidget()
 		{
-			this.m_E_ActivityLoginItemsLoopVerticalScrollRect = null;
 			this.uiTransform = null;
 		}
 
-		private LoopVerticalScrollRect m_E_ActivityLoginItemsLoopVerticalScrollRect = null;
 		public Transform uiTransform = null;
 	}
 }
