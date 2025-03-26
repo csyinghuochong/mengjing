d6c2fc5c937c9a5aaad5841dd44f4a4acbed9b89
FortuneCat0v0 <1142094319@qq.com>
2025-03-26 15:47:03 +0800

    滚动列表有时候会突然消失，其中一个原因，当Scroll是隐藏时，给Scroll添加Item后，content的高度不会刷新(一开始为0)，之后显示Scroll，如果滚动下鼠标滚轮，循环列表可能会判断回收item。处理方式，要在为Scroll添加Item之前，显示Scroll和其父物体

--- Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/DlgSkill/ES_SkillMake.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/DlgSkill/ES_SkillMake.cs
@@ -23,6 +23,11 @@ namespace ET.Client
 		public Dictionary<int, EntityRef<Scroll_Item_CommonItem>> ScrollItemCommonItems;
 		public bool IsHoldDown = false;
 		public int PlanMelt = 1;
+
+		public EntityRef<ES_RewardList> RewardList_1;
+		public EntityRef<ES_RewardList> RewardList_2;
+		public EntityRef<ES_RewardList> RewardList_3;
+		public EntityRef<ES_RewardList> RewardList_6;
 		
 		public RectTransform EG_RightRectTransform
      	{
