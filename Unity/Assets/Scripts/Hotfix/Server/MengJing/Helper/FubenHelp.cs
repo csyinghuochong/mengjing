e22195017b982a286515956e0989e37b03194424
tangchunguang <394266315@qq.com>
2025-03-31 16:18:26 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/Helper/FubenHelp.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Helper/FubenHelp.cs
@@ -328,11 +328,13 @@ namespace ET.Server
 				long serverNow = TimeHelper.ServerNow();
 				long seasonBossTime = numericComponent.GetAsLong(NumericType.SeasonBossRefreshTime);
 				int sessonBossFuben = numericComponent.GetAsInt(NumericType.SeasonBossFuben);
+				int bosslv = numericComponent.GetAsInt(NumericType.SeasonBossLeve);
+				int bossId = ConfigData.SeasonBossList[bosslv].KeyId;
 				if (seasonBossTime > 0 && serverNow > seasonBossTime && mapComponent.SceneId == sessonBossFuben)
 				{
 					KeyValuePairInt keyValuePairInt = new KeyValuePairInt();
 					keyValuePairInt.KeyId = RandomHelper.RandomNumber(0, monsters.Length);
-					keyValuePairInt.Value = ConfigData.SeasonBossId;
+					keyValuePairInt.Value = bossId;
 					randomMonsterList.Add(keyValuePairInt);
 				}
 			}
