bbe06fde594a6b296944f136c04eeea164afd953
FortuneCat0v0 <1142094319@qq.com>
2025-04-02 19:44:45 +0800

    临时屏蔽这个传承列表、这里支持上下收缩 主城神器商人

--- Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/Other/UIShenQiChapterComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UIBehaviour/Other/UIShenQiChapterComponent.cs
@@ -7,6 +7,10 @@ namespace ET.Client
     [ChildOf]
     public class UIShenQiChapterComponent: Entity, IAwake<GameObject>
     {
+        public float Height;
+        public bool IsExpand;
+        public GameObject Button_Click;
+        public GameObject Checkmark;
         public GameObject ItemNode;
         public GameObject Text_Name;
         public GameObject GameObject;
