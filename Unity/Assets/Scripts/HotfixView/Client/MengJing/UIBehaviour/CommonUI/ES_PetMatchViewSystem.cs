2e2410c93aed0050ddbac5824211cf14afade68b
tangchunguang <394266315@qq.com>
2025-04-02 11:56:11 +0800

    no message

---
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIBehaviour/CommonUI/ES_PetMatchViewSystem.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET.Client
+{
+	[EntitySystemOf(typeof(ES_PetMatch))]
+	[FriendOfAttribute(typeof(ES_PetMatch))]
+	public static partial class ES_PetMatchSystem 
+	{
+		[EntitySystem]
+		private static void Awake(this ES_PetMatch self,Transform transform)
+		{
+			self.uiTransform = transform;
+		}
+
+		[EntitySystem]
+		private static void Destroy(this ES_PetMatch self)
+		{
+			self.DestroyWidget();
+		}
+	}
+
+
+}
