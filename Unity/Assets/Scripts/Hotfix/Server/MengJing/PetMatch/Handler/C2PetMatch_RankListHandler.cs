4cc8ad515badf7415de8d4164a40b37a347f0f80
tangchunguang <394266315@qq.com>
2025-04-03 14:23:49 +0800

    no message

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/PetMatch/Handler/C2PetMatch_RankListHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/PetMatch/Handler/C2PetMatch_RankListHandler.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace ET.Server
 {
 
@@ -8,7 +10,18 @@ namespace ET.Server
         {
             PetMatchSceneComponent soloSceneComponent = scene.GetComponent<PetMatchSceneComponent>();
 
-            response.PetMatchPlayerInfoList = soloSceneComponent.GetSoloResult();
+            List<PetMatchPlayerInfo> petMatchPlayerInfos =  soloSceneComponent.GetSoloResult();
+            response.PetMatchPlayerInfoList .AddRange( petMatchPlayerInfos );
+
+            if (response.PetMatchPlayerInfoList.Count == 0)
+            {
+                PetMatchPlayerInfo petMatchPlayerInfo = PetMatchPlayerInfo.Create();
+                petMatchPlayerInfo.UnitId = 1;
+                petMatchPlayerInfo.Name = "XX";
+                petMatchPlayerInfo.Occ = 2;
+                petMatchPlayerInfo.Score = 100;
+                petMatchPlayerInfo.RankId = 3;
+            }
 
             await ETTask.CompletedTask;
         }
