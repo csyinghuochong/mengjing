0f260e8dd7b9b67e0a81f3bbeeb58ef887dd644c
tangchunguang <394266315@qq.com>
2025-04-18 15:11:19 +0800

    no message

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_ChengJiuJinglingItemViewSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UIItemBehaviour/Item_ChengJiuJinglingItemViewSystem.cs
@@ -26,10 +26,25 @@ namespace ET.Client
             self.E_NameText.text = jingLingConfig.Name;
             
             self.ES_ModelShow.ShowOtherModel("JingLing/" + jingLingConfig.Assets).Coroutine();
-            self.ES_ModelShow.SetCameraPosition(new Vector3(0f, 40f, 200f));
-            // self.ES_ModelShow.SetRootPosition(new Vector2(jingLingConfig.Id % 10 * 1000, 0));
-            self.ES_ModelShow.SetModelParentRotation(Quaternion.Euler(0f, -45f, 0f));
-
+            
+            string[] strs = null;
+            if (!CommonHelp.IfNull(jingLingConfig.ModelShowPosi))
+            {
+                strs = jingLingConfig.ModelShowPosi.Split(',');
+            }
+            if (strs != null && strs.Length >= 5)
+            {
+                self.ES_ModelShow.Camera.fieldOfView = float.Parse(strs[3]);
+                self.ES_ModelShow.SetCameraPosition(new Vector3(float.Parse(strs[0]), float.Parse(strs[1]), float.Parse(strs[2])));
+                self.ES_ModelShow.RotationY = float.Parse(strs[4]); 
+            }
+            else
+            {
+                self.ES_ModelShow.SetCameraPosition(new Vector3(0f, 40f, 200f));
+                self.ES_ModelShow.SetModelParentRotation(Quaternion.Euler(0f, -45f, 0f));
+            }
+            //self.ES_ModelShow.SetRootPosition(new Vector2(jingLingConfig.Id % 10 * 1000, 0));
+            
             bool active = jingLingInfo.IsActive == 1;
             self.E_ActivatedText.text = active ? "已激活" : "未激活";
             CommonViewHelper.SetRawImageGray(self.Root(), self.ES_ModelShow.E_RenderRawImage.gameObject, !active);
