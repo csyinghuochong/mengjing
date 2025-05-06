de2aebb616161c4a2d3bdb1a657762df5a54b920
FortuneCat0v0 <1142094319@qq.com>
2025-05-06 17:10:14 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgMain/DlgMainSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/DlgMain/DlgMainSystem.cs
@@ -1449,8 +1449,8 @@ namespace ET.Client
 
                 self.View.ES_MainSkill.OnEnterScene(unit, petId);
                 self.View.ES_MainSkill.OnPetFightSwitch(petId);
-                self.View.E_TextPetSwitch.text = "600";
-                self.MainPetSwitchEndTime = TimeHelper.ServerNow() + TimeHelper.Second * 600;
+                self.View.E_TextPetSwitch.text = "10";
+                self.MainPetSwitchEndTime = TimeHelper.ServerNow() + TimeHelper.Second * 10;
                 self.MainPetSwitchTimer = root.GetComponent<TimerComponent>().NewRepeatedTimer(1000, TimerInvokeType.MainPetSwitchTimer, self);
             }
             else
