318be8c0d7cb607dc3796687e82e9ce1cbbe0aa8
tangchunguang2 <2322657661@qq.com>
2025-05-08 10:21:37 +0800

    no message

---
+++ Unity/Assets/Scripts/Loader/MonoBehaviour/RenderScaleController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+// 通过代码动态修改（示例）
+
+public class RenderScaleController : MonoBehaviour
+{
+    public UniversalRenderPipelineAsset urpAsset;
+    
+    void Start()
+    {
+        urpAsset = GraphicsSettings.renderPipelineAsset as UniversalRenderPipelineAsset;
+
+        if (urpAsset != null)
+        {
+            Debug.Log("成功获取 URP Asset: " + urpAsset.name);
+            // 在此处使用 urpAsset 进行配置
+        }
+        else
+        {
+            Debug.LogError("当前未使用 URP 或未分配 URP Asset！");
+            
+            urpAsset = QualitySettings.renderPipeline as UniversalRenderPipelineAsset;
+
+            if (urpAsset != null)
+            {
+                Debug.Log("当前质量级别的 URP Asset: " + urpAsset.name);
+            }
+            else
+            {
+                Debug.LogError("当前质量级别未分配 URP Asset！");
+            }
+        }
+        
+        
+        // 设置渲染分辨率为原始分辨率的 50%
+        urpAsset.renderScale = 0.5f; 
+        
+        // 强制更新
+        QualitySettings.renderPipeline = urpAsset;
+    }
+}
