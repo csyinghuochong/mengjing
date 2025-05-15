57621d9169b8a7c2453ae1bf4806512705780ad2
FortuneCat0v0 <1142094319@qq.com>
2025-05-15 15:01:39 +0800

    新增工具： shader同名查找，同名的打印出来

---
+++ Unity/Assets/Scripts/Editor/Custom/FindDuplicateShaders.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace ET.Client
+{
+    public static class FindDuplicateShaders
+    {
+        [MenuItem("Tools/Shader同名查找")]
+        public static void FindDuplicates()
+        {
+            string[] guids = AssetDatabase.FindAssets("t:Shader");
+            Dictionary<string, List<string>> shaderMap = new Dictionary<string, List<string>>();
+
+            foreach (string guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                Shader shader = AssetDatabase.LoadAssetAtPath<Shader>(path);
+                if (shader == null) continue;
+
+                string name = shader.name;
+                if (!shaderMap.ContainsKey(name))
+                    shaderMap[name] = new List<string>();
+                shaderMap[name].Add(path);
+            }
+
+            var duplicates = shaderMap.Where(kvp => kvp.Value.Count > 1).ToList();
+
+            if (duplicates.Count == 0)
+            {
+                Debug.Log("<color=green>没有找到同名Shader.</color>");
+                return;
+            }
+
+            Debug.Log($"<color=orange>找到{duplicates.Count}个同名Shader name(s):</color>\n");
+
+            int index = 1;
+            foreach (var kvp in duplicates)
+            {
+                Debug.Log($"<color=yellow>▶({index}) Shader Name: {kvp.Key}</color>");
+                foreach (string path in kvp.Value)
+                {
+                    Debug.Log($"    <color=cyan>• {path}</color>");
+                }
+
+                index++;
+            }
+        }
+    }
+}
