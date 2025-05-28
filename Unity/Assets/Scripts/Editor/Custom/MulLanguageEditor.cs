ec1b460fbc6eecd8d72a22d3f5b6a8fb07e910d8
FortuneCat0v0 <1142094319@qq.com>
2025-05-28 15:40:03 +0800

    提取预制体和脚本中，中文字符串

--- Unity/Assets/Scripts/Editor/Custom/MulLanguageEditor.cs
+++ Unity/Assets/Scripts/Editor/Custom/MulLanguageEditor.cs
@@ -14,7 +14,7 @@ namespace ET.Client
         private static string UIPrefabPath = Application.dataPath + "/Bundles/UI";
 
         //脚本的文件夹目录
-        private static string ScriptPath = Application.dataPath + "/Codes";
+        private static string ScriptPath = Application.dataPath + "/Scripts";
 
         //导出的中文KEY路径
         private static string OutPath = Application.dataPath + "/out.txt";
@@ -34,9 +34,6 @@ namespace ET.Client
 
             //提取CS中的中文
             staticWriteText += "----------------Script----------------------\n";
-
-            ScriptPath = Application.dataPath.Substring(0, Application.dataPath.Length - 6);
-            ScriptPath = ScriptPath + "Codes";
             LoadDiectoryCS(new DirectoryInfo(ScriptPath));
 
             //最终把提取的中文生成出来
@@ -81,18 +78,23 @@ namespace ET.Client
             foreach (FileInfo files in fileInfos)
             {
                 string path = files.FullName;
-                //string assetPath = path.Substring(path.IndexOf("Assets/"));
-                //int index = Application.dataPath.Length - 6; //path.IndexOf("Assets");
-                //string assetPath = path.Substring(index);
-                //TextAsset textAsset = AssetDatabase.LoadAssetAtPath(assetPath, typeof(TextAsset)) as TextAsset;
-                string text = File.ReadAllText(path); // textAsset.text;
+                string text = File.ReadAllText(path);
 
                 //用正则表达式把代码里面两种字符串中间的字符串提取出来。
-                Regex reg = new Regex("(?<=StrUtil.GetText\\s*\\(\\s*\"\\s*)[\\s\\S]*?(?=\\s*\")");
+                // Regex reg = new Regex("(?<=StrUtil.GetText\\s*\\(\\s*\"\\s*)[\\s\\S]*?(?=\\s*\")");
+                Regex reg = new Regex("\"(.*?)\"", RegexOptions.Compiled);
                 MatchCollection mc = reg.Matches(text);
+                Regex chineseCharRegex = new Regex("[\u4e00-\u9fa5]", RegexOptions.Compiled);
                 foreach (Match m in mc)
                 {
-                    string format = m.Value;
+                    string format = m.Groups[1].Value;
+                    
+                    // 判断是否包含中文字符
+                    if (!chineseCharRegex.IsMatch(format))
+                    {
+                        continue;
+                    }
+
                     if (!Localization.Contains(format) && !string.IsNullOrEmpty(format))
                     {
                         Localization.Add(format);
@@ -110,9 +112,17 @@ namespace ET.Client
                 //因为这里是写例子，所以我用的是UILabel 
                 //这里应该是写你用于图文混排的脚本。
                 Text label = chind.GetComponent<Text>();
+                Regex chineseCharRegex = new Regex("[\u4e00-\u9fa5]", RegexOptions.Compiled);
                 if (label != null)
                 {
                     string text = label.text;
+                    
+                    // 判断是否包含中文字符
+                    if (!chineseCharRegex.IsMatch(text))
+                    {
+                        continue;
+                    }
+                    
                     if (!Localization.Contains(text) && !string.IsNullOrEmpty(text))
                     {
                         Localization.Add(text);
